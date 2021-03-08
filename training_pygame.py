a0e385187d8603cc1f880e1f77f53d2929d813c0
EdMix <ua.capitan@gmail.com>
2021-03-08 18:56:43 +0200

    Keyboard events

--- training_pygame.py
+++ training_pygame.py
@@ -34,6 +34,7 @@ import sys
 #     clock.tick(FPS)
 
 # Модуль pygame.draw
+
 # pygame.init()
 
 # FPS = 60
@@ -59,6 +60,78 @@ import sys
 #     surface_main.fill(WHITE)
 #     pygame.draw.circle(surface_main, ORANGE, (x,y), r)
 #     pygame.display.update()
+#     if x >= WIN_WIDTH + r:
+#         x = 0 - r
+#     else:
+#         x += 2
+#     clock.tick(FPS)
+
+# Задание
+
+# pygame.init()
+
+# FPS = 60
+# WHITE = (255,255,255)
+# ORANGE = (255,150,100)
+# WIN_WIDTH = 400
+# WIN_HEIGHT = 100
+
+# surface_main = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+
+# clock = pygame.time.Clock()
+
+# r = 30
+# x = 0 - r
+# y = WIN_HEIGHT // 2
+
+# while True:
+#     pygame.display.set_caption('Pygame')
+#     for i in pygame.event.get():
+#         if i.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#     surface_main.fill(WHITE)
+#     pygame.draw.rect(surface_main, ORANGE, (x, y, r, r))
+#     pygame.display.update()
+#     if x >= WIN_WIDTH + r:
+#         x = 0 - r
+#     else:
+#         x += 2
+#     clock.tick(FPS)
+
+# События клавиатуры
+
+# pygame.init()
+
+# FPS = 60
+# WHITE = (255,255,255)
+# ORANGE = (255,150,100)
+# WIN_WIDTH = 400
+# WIN_HEIGHT = 100
+
+# surface_main = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+
+# clock = pygame.time.Clock()
+
+# r = 30
+# x = 0 - r
+# y = WIN_HEIGHT // 2
+
+# while True:
+#     pygame.display.set_caption('Pygame')
+#     for i in pygame.event.get():
+#         if i.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         elif i.type == pygame.KEYUP:
+#             reverse_move = True
+#     surface_main.fill(WHITE)
+#     pygame.draw.rect(surface_main, ORANGE, (x, y, r, r))
+#     pygame.display.update()
+#     if x > 0 and reverse_move:
+#         x -= 4
+#     elif x <= 0:
+#         reverse_move = False
 #     if x >= WIN_WIDTH + r:
 #         x = 0 - r
 #     else:
