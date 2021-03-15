edb98873286668d3911b4efa3ac7a37a435f0050
EdMix <ua.capitan@gmail.com>
2021-03-15 14:35:11 +0200

    Vote page

--- training_django/mysite/polls/views.py
+++ training_django/mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Question, Users
 from django.http import Http404
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,11 +16,22 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'You are looking at the results of question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('You are voting on question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'Error'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=[question.id]))
 
 def users(request):
     return HttpResponse('Users')
