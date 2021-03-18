45c66d21af4f927392a5d6e1da4ff78c938ace3f
EdMix <ua.capitan@gmail.com>
2021-03-19 01:00:56 +0200

    Form

--- training_django/my_site_3/main/views.py
+++ training_django/my_site_3/main/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import Task_form
 
 
 def index(request):
     tasks = Task.objects.all()
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title':'Main page', 'tasks':tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = Task_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            error = 'Error, form not valid'
+
+    form = Task_form()
+    context = {
+        'form':form,
+        'error':error
+    }
+    return render(request, 'main/create.html', context)
