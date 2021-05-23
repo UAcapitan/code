b93646ad3fac50eadf4ccbd20f6a38c0b9d5d054
EdMix <ua.capitan@gmail.com>
2021-05-23 15:10:55 +0300

    DMS - add view and update urls

--- training/training_django/my_site_5/main_app/views.py
+++ training/training_django/my_site_5/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse('Hello')
-    # return render(request, 'main_app/index.html', [])
+    return render(request, 'main_app/index.html')
+
+def articles(request):
+    return render(request, 'articles/index.html')
