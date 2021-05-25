d795c0f2bcdc8b259d3acbb7d39932617b76f693
EdMix <ua.capitan@gmail.com>
2021-05-25 22:31:13 +0300

    Task - change of variables

--- training/training_django/my_site_5/main_app/urls.py
+++ training/training_django/my_site_5/main_app/urls.py
@@ -7,4 +7,5 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("articles", views.articles, name="articles"),
+    path("form", views.form, name="form"),
 ]
--- training/training_django/my_site_5/main_app/views.py
+++ training/training_django/my_site_5/main_app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ArticleForm
 
 def index(request):
     return render(request, 'main_app/index.html')
 
 def articles(request):
     return render(request, 'articles/index.html')
+
+def form(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect('')
+
+    else:
+        form = ArticleForm()
+
+    return render(request, 'form/index.html', {'form': form})
