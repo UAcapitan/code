e28f4e3fb4de3bea8b771030644b4d1a588dea61
EdMix <ua.capitan@gmail.com>
2021-06-04 02:14:28 +0300

    Update statistics_from_github - add date

--- interesting_projects/Statistics_github/statistics_from_github.py
+++ interesting_projects/Statistics_github/statistics_from_github.py
@@ -1,3 +1,7 @@
+# --------------------------------------
+# Development start date: Apr 2021
+# --------------------------------------
+
 import requests
 from bs4 import BeautifulSoup
 import datetime
