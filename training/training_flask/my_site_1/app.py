0a1c26e3f3b61cdfb48a3fa621d6b091b7b334e7
EdMix <ua.capitan@gmail.com>
2021-07-04 22:30:18 +0300

    1F - update templates and add model

--- training/training_flask/my_site_1/app.py
+++ training/training_flask/my_site_1/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 @app.route('/')
-@app.route('/home')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return 'About'
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'Hello ' + name + ' ' + str(id)
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug='True')
