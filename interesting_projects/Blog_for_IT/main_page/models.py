c44e704020bb9c0477a564d7d9c7d668599418cc
EdMix <ua.capitan@gmail.com>
2021-07-05 23:05:22 +0300

    BlogIT - add date links

--- interesting_projects/Blog_for_IT/main_page/models.py
+++ interesting_projects/Blog_for_IT/main_page/models.py
@@ -4,3 +4,4 @@ class Article(models.Model):
     name = models.CharField(max_length=100)
     text = models.TextField()
     author = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
