28efd3fc563cfdb08958a48bb992b85319e4c95c
EdMix <ua.capitan@gmail.com>
2021-07-07 00:00:21 +0300

    BlogIT - create create_article page

--- interesting_projects/Blog_for_IT/main_page/urls.py
+++ interesting_projects/Blog_for_IT/main_page/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-article/', views.create_article, name='create_article'),
 ]
--- interesting_projects/Blog_for_IT/main_page/views.py
+++ interesting_projects/Blog_for_IT/main_page/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/main.html', {})
+    return render(request, 'main.html', {})
+
+def create_article(request):
+    return render(request, 'create_article.html', {})
 
 
