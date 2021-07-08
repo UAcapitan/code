8dbfa641b60fa76f500024cdfff5d0c9c0aeb455
EdMix <ua.capitan@gmail.com>
2021-07-08 22:11:13 +0300

    BlogIT - add field image in model

--- interesting_projects/Blog_for_IT/main_page/models.py
+++ interesting_projects/Blog_for_IT/main_page/models.py
@@ -5,3 +5,4 @@ class Article(models.Model):
     text = models.TextField()
     author = models.CharField(max_length=50)
     date = models.DateField(auto_now=True)
+    image = models.CharField(max_length=200, default='')
