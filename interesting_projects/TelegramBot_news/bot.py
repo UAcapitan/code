a892346ad1e21b6ebd84b143367da88defe33af3
EdMix <ua.capitan@gmail.com>
2021-07-17 00:41:35 +0300

    TBN - create first function

--- interesting_projects/TelegramBot_news/bot.py
+++ interesting_projects/TelegramBot_news/bot.py
@@ -1,5 +1,13 @@
 import telebot
 from bs4 import BeautifulSoup
 
-TOKEN = ''
+TOKEN = '1912816248:AAFfHtIKznlhFFMC-k4OfmKEZhRwI4gK2sc'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
 
