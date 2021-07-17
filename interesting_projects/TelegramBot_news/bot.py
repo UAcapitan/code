7b7a32164f95b7758b1c3ba77bd28ff965b8d455
EdMix <ua.capitan@gmail.com>
2021-07-18 00:43:57 +0300

    TBN - add url parsing

--- interesting_projects/TelegramBot_news/bot.py
+++ interesting_projects/TelegramBot_news/bot.py
@@ -1,3 +1,8 @@
+# --------------------------------------
+# Development start date: 16 Jul 2021
+# --------------------------------------
+
+import requests
 import telebot
 from bs4 import BeautifulSoup
 
@@ -7,6 +12,9 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    url = 'https://habr.com/ru/search/?q=Python&target_type=posts&order=date'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
     bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop=True)
