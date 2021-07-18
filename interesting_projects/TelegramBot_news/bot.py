66735aef17aa2f887a6f84f66559ce116bfdf8d0
EdMix <ua.capitan@gmail.com>
2021-07-18 13:05:18 +0300

    TBN - update open site

--- interesting_projects/TelegramBot_news/bot.py
+++ interesting_projects/TelegramBot_news/bot.py
@@ -2,7 +2,7 @@
 # Development start date: 16 Jul 2021
 # --------------------------------------
 
-import requests
+import urllib2
 import telebot
 from bs4 import BeautifulSoup
 
@@ -13,8 +13,8 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     url = 'https://habr.com/ru/search/?q=Python&target_type=posts&order=date'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
     bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop=True)
