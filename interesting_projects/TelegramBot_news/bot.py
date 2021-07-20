bbc3ab56f82134782a6a5cdc3bfd58430c8312e8
EdMix <ua.capitan@gmail.com>
2021-07-21 00:51:41 +0300

    TBN - create buttons menu

--- interesting_projects/TelegramBot_news/bot.py
+++ interesting_projects/TelegramBot_news/bot.py
@@ -11,39 +11,55 @@ TOKEN = '1912816248:AAFfHtIKznlhFFMC-k4OfmKEZhRwI4gK2sc'
 
 bot = telebot.TeleBot(TOKEN)
 
-list_buttons = ['News', 'News Python', 'News Habr']
+list_buttons = ['Habr News Python', 'Habr All', 'Habr Python', '/back']
+
+@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['back'])
+def start_bot(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('Habr')
+    markup.add(button1)
+    bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
 
-    if message.text == 'Buttons':
+    if message.text == 'Habr':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('News')
-        button2 = types.KeyboardButton('News Habr')
-        button3 = types.KeyboardButton('News Python')
+        button1 = types.KeyboardButton(list_buttons[0])
+        button2 = types.KeyboardButton(list_buttons[1])
+        button3 = types.KeyboardButton(list_buttons[2])
+        button4 = types.KeyboardButton(list_buttons[3])
         markup.add(button1)
         markup.add(button2)
         markup.add(button3)
+        markup.add(button4)
         bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
     else:
         if message.text in list_buttons:
-            if message.text == 'News':
+            if message.text == list_buttons[0]:
                 url = 'https://habr.com/ru/search/?q=Python&target_type=posts&order=date'
                 tag = 'a'
                 class_tag = 'tm-article-snippet__title-link'
                 url_site = 'habr.com'
             
-            elif message.text == 'News Habr':
+            elif message.text == list_buttons[1]:
                 url = 'https://habr.com/ru/all/'
                 tag = 'a'
                 class_tag = 'tm-article-snippet__title-link'
                 url_site = 'habr.com'
 
-            elif message.text == 'News Python':
+            elif message.text == list_buttons[2]:
                 url = 'https://habr.com/ru/hub/python/'
                 tag = 'a'
                 class_tag = 'tm-article-snippet__title-link'
                 url_site = 'habr.com'
+
+            # elif message.text == list_buttons[3]:
+            #     url = 'https://dailycoding.io/'
+            #     tag = 'a'
+            #     class_tag = 'title'
+            #     url_site = 'https://dailycoding.io/'
 
             page = urllib2.urlopen(url)
             soup = BeautifulSoup(page, 'html.parser')
