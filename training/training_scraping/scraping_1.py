65042c9c45f78bffc2b92c5e51cf9cb672ba29e5
EdMix <ua.capitan@gmail.com>
2021-07-21 19:01:34 +0300

    TSP - scraping site test

--- training/training_scraping/scraping_1.py
+++ training/training_scraping/scraping_1.py
@@ -1,5 +1,10 @@
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 
-url = 'https://dailycoding.io/'
+url = 'https://habr.com/ru/all/'
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+name_box = soup.find('a', attrs={'class': 'tm-article-snippet__title-link'})
+name = name_box.text.strip()
+print(name)
 
