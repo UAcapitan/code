036ca781b7fd71f70b8297e6ca7ca2d86df591ff
EdMix <ua.capitan@gmail.com>
2021-07-23 13:31:29 +0300

    TBN - delete buttons menu

--- interesting_projects/TelegramBot_news/bot.py
+++ interesting_projects/TelegramBot_news/bot.py
@@ -12,8 +12,6 @@ TOKEN = '1912816248:AAFfHtIKznlhFFMC-k4OfmKEZhRwI4gK2sc'
 bot = telebot.TeleBot(TOKEN)
 
 list_buttons = ['Habr News Python', 'Habr All', 'Habr Python', '/back']
-list_buttons_video = ['Programming', 'Python', 'Web', '/back']
-list_buttons_video = ['Courses', '/back']
 
 @bot.message_handler(commands=['start'])
 @bot.message_handler(commands=['back'])
@@ -41,17 +39,6 @@ def send_text(message):
         markup.add(button3)
         markup.add(button4)
         bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
-    elif message.text == 'Video':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton(list_buttons_video[0])
-        button2 = types.KeyboardButton(list_buttons_video[1])
-        button3 = types.KeyboardButton(list_buttons_video[2])
-        button4 = types.KeyboardButton(list_buttons_video[3])
-        markup.add(button1)
-        markup.add(button2)
-        markup.add(button3)
-        markup.add(button4)
-        bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
     else:
         if message.text in list_buttons:
             if message.text == list_buttons[0]:
@@ -71,12 +58,6 @@ def send_text(message):
                 tag = 'a'
                 class_tag = 'tm-article-snippet__title-link'
                 url_site = 'habr.com'
-
-            # elif message.text == list_buttons[3]:
-            #     url = 'https://dailycoding.io/'
-            #     tag = 'a'
-            #     class_tag = 'title'
-            #     url_site = 'https://dailycoding.io/'
 
             page = urllib2.urlopen(url)
             soup = BeautifulSoup(page, 'html.parser')
