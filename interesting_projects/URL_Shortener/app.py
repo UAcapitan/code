4f3bd7794092535b2a3dd676c85e549fc9591727
EdMix <ua.capitan@gmail.com>
2021-08-04 03:38:47 +0300

    URL - create db model

--- interesting_projects/URL_Shortener/app.py
+++ interesting_projects/URL_Shortener/app.py
@@ -4,6 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full = db.Column(db.String(300), nullable=False)
+    short = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' % self.short
 
 @app.route('/')
 def index():
