654188d06857e3e66c57841c3fb957b3286f525c
EdMix <ua.capitan@gmail.com>
2021-08-07 01:07:50 +0300

    TK - update text font

--- training/training_kivy/main.py
+++ training/training_kivy/main.py
@@ -3,7 +3,7 @@ from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        return Button(text='This is my first button')
+        return Button(text='This is my first button', font_size=30)
 
 if __name__ == '__main__':
     MyApp().run()
