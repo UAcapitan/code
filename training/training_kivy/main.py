14bedde82415ca17d7f8d922bf18b5d2f5f2e89c
EdMix <ua.capitan@gmail.com>
2021-08-08 01:13:58 +0300

    TK - function for button

--- training/training_kivy/main.py
+++ training/training_kivy/main.py
@@ -3,7 +3,14 @@ from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        return Button(text='This is my first button', font_size=30)
+        return Button(
+            text='This is my first button', 
+            font_size=30,
+            on_press=self.btn_press
+        )
+
+    def btn_press(self, instance):
+        print('Work')
 
 if __name__ == '__main__':
     MyApp().run()
