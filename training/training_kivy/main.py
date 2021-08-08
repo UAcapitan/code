6d92b4708e1675c6f434024e04bb1699c0780f4a
EdMix <ua.capitan@gmail.com>
2021-08-08 16:08:06 +0300

    TK - update this button

--- training/training_kivy/main.py
+++ training/training_kivy/main.py
@@ -6,11 +6,14 @@ class MyApp(App):
         return Button(
             text='This is my first button', 
             font_size=30,
-            on_press=self.btn_press
+            on_press=self.btn_press,
+            background_color=[0,.5,0,1],
+            background_normal=''
         )
 
     def btn_press(self, instance):
         print('Work')
+        instance.text = 'Work'
 
 if __name__ == '__main__':
     MyApp().run()
