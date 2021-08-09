8f429fd7ee2980574e31c0614c1e4874dbb2b23e
EdMix <ua.capitan@gmail.com>
2021-08-10 02:01:27 +0300

    TLK - add new button

--- training_libs/training_kivy/main.py
+++ training_libs/training_kivy/main.py
@@ -1,4 +1,17 @@
 from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(
+            text = 'Text',
+            on_press = self.click,
+            background_color = [0, 1, 0, 1]
+        )
+
+    def click(self,i):
+        print('Work')
+        i.text = 'Test'
 
 if __name__ == '__main__':
-    MyApp.run()
+    MyApp().run()
