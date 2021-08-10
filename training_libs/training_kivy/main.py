1c399f92469993ec036a57a65e5398efcc824042
EdMix <ua.capitan@gmail.com>
2021-08-11 00:32:14 +0300

    TLK - update function button

--- training_libs/training_kivy/main.py
+++ training_libs/training_kivy/main.py
@@ -1,17 +1,22 @@
 from kivy.app import App
 from kivy.uix.button import Button
 
+n = 0
+
 class MyApp(App):
     def build(self):
+        global n
         return Button(
-            text = 'Text',
+            text = str(n),
             on_press = self.click,
             background_color = [0, 1, 0, 1]
         )
 
     def click(self,i):
+        global n
         print('Work')
-        i.text = 'Test'
+        n += 1
+        i.text = str(n)
 
 if __name__ == '__main__':
     MyApp().run()
