cef1cc8ac8119b98c4aac66b364087d4a968a94f
EdMix <ua.capitan@gmail.com>
2021-08-12 00:30:28 +0300

    TLK - add floatlayout widget

--- training_libs/training_kivy/main.py
+++ training_libs/training_kivy/main.py
@@ -1,16 +1,21 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
 n = 0
 
 class MyApp(App):
     def build(self):
+
+        f1 = FloatLayout(size=(300,300))
         global n
-        return Button(
+        f1.add_widget(Button(
             text = str(n),
             on_press = self.click,
             background_color = [0, 1, 0, 1]
-        )
+        ))
+
+        return f1
 
     def click(self,i):
         global n
