c1d899832b4a5bfd04782911130d58d51da4b591
EdMix <ua.capitan@gmail.com>
2021-09-10 00:42:30 +0300

    TF - add db file

--- training_libs/flask/app.py
+++ training_libs/flask/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    cost = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 @app.route('/')
 def index():
