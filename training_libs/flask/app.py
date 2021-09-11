e3a166f30228c0f4be04b1b99cb1fc0c7f11bb1f
EdMix <ua.capitan@gmail.com>
2021-09-11 17:33:14 +0300

    TF - add little changes

--- training_libs/flask/app.py
+++ training_libs/flask/app.py
@@ -12,6 +12,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     cost = db.Column(db.Integer)
+    decription = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return '<Article %r>' % self.id
