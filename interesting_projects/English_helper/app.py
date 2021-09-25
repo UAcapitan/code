a7a8e4abb41995642325c42a490951d3eb4ae58f
EdMix <ua.capitan@gmail.com>
2021-09-25 12:42:57 +0300

    EH - window

--- interesting_projects/English_helper/app.py
+++ interesting_projects/English_helper/app.py
@@ -2,3 +2,17 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import sys
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle('English helper')
+    window.setGeometry(300,250,450,300)
+
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    application()
+
