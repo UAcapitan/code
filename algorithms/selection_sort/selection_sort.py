fd05d4d8cfc9c31d62655dd3de7440301218d05d
EdMix <ua.capitan@gmail.com>
2021-09-28 00:38:41 +0300

    A - working selection sort

--- algorithms/selection_sort/selection_sort.py
+++ algorithms/selection_sort/selection_sort.py
@@ -1,1 +1,12 @@
 my_list = [1,0,3,6,5,7,9,8,2,4]
+
+def selection_sort(l_old):
+    l = []
+    for i in range(len(l_old)):
+        m = min(l_old)
+        n = l_old.index(m)
+        l_old.pop(n)
+        l.append(m)
+    return l
+
+print(selection_sort(my_list))
