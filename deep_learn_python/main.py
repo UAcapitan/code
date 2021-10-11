d6f30b47c2daa5e44397d2adb99b5bb98c546a5d
EdMix <ua.capitan@gmail.com>
2021-10-11 23:53:49 +0300

    DLP - filter, quit, repr

--- deep_learn_python/main.py
+++ deep_learn_python/main.py
@@ -342,3 +342,28 @@ class Person:
 # print(p.name)
 # p.name = 'John'
 # del p.name
+
+# filter --------------------------------------------------
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def check_even(number):
+    if number % 2 == 0:
+          return True  
+    return False
+
+even_numbers_iterator = filter(check_even, numbers)
+even_numbers = list(even_numbers_iterator)
+# print(even_numbers)
+
+# quit ----------------------------------------------------
+
+# input()
+# quit()
+
+# repr ----------------------------------------------------
+
+numbers = [1, 2, 3, 4, 5]
+
+printable_numbers = repr(numbers)
+# print(printable_numbers)
