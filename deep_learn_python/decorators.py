fde188b699a173c3d995406945510a4331d90af6
EdMix <ua.capitan@gmail.com>
2021-10-14 15:56:58 +0300

    DLP - decorator

--- deep_learn_python/decorators.py
+++ deep_learn_python/decorators.py
@@ -19,8 +19,21 @@ class User():
         print('Delete name')
         del self._name
 
-user = User('Max')
-print(user.name)
-user.name = 'Maxim'
-del user.name
+# user = User('Max')
+# print(user.name)
+# user.name = 'Maxim'
+# del user.name
+
+# decorator ----------------------------------------------
+
+def decorator_list(fnc):
+    def inner(list_of_tuples):
+        return [fnc(val[0], val[1]) for val in list_of_tuples]
+    return inner
+
+@decorator_list
+def add_together(a, b):
+    return a + b
+
+# print(add_together([(1, 3), (3, 17), (5, 5), (6, 7)]))
 
