1e7a04dccd16bf6bc08e6f7dd256ce129bcd1deb
EdMix <ua.capitan@gmail.com>
2021-10-16 00:12:56 +0300

    DLP - methods of dict, part 1

--- deep_learn_python/dict.py
+++ deep_learn_python/dict.py
@@ -3,3 +3,24 @@ my_dict = {
     '2':'b',
     '3':'c'
 }
+
+print(my_dict)
+
+my_dict.clear()
+print(my_dict)
+
+my_dict['1'] = 'a'
+my_dict['2'] = 'b'
+my_dict['3'] = 'c'
+print(my_dict)
+
+md = my_dict.copy()
+print(md)
+
+print(my_dict.fromkeys('1', 'a'))
+
+print(my_dict.get('1'))
+
+print(my_dict.items())
+
+print(my_dict.keys())
