a1bd505a444697107207ee3f5590bef0c5772338
EdMix <ua.capitan@gmail.com>
2021-10-16 03:28:11 +0300

    DLP - API tokens and auth url

--- deep_learn_python/api.py
+++ deep_learn_python/api.py
@@ -1,1 +1,5 @@
-TOKEN = ''
+import requests
+
+URL = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+TOKEN = 'ZmViNDE5MzQtNDEyZS00NGY2LTlhY2QtZTdkNTY4MmYyY2MxOmExMmY2MGZmN2FiMjRlODRiZTRhYTIxN2RmMjI0NjZl'
+
