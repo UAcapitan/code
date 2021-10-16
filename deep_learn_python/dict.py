c979b791dd80015f1c9e40d96ff6cc2942273953
EdMix <ua.capitan@gmail.com>
2021-10-16 03:21:01 +0300

    DLP - methods of dict

--- deep_learn_python/dict.py
+++ deep_learn_python/dict.py
@@ -24,3 +24,16 @@ print(my_dict.get('1'))
 print(my_dict.items())
 
 print(my_dict.keys())
+
+my_dict.pop('2')
+print(my_dict)
+
+my_dict.popitem()
+print(my_dict)
+
+print(my_dict.setdefault('1'))
+
+my_dict.update({'2':'b'})
+print(my_dict)
+
+print(my_dict.values())
