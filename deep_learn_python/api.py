5fea6ec034e7bac84630f970a5ca5f754e901540
EdMix <ua.capitan@gmail.com>
2021-10-16 13:55:53 +0300

    DLP - work with api

--- deep_learn_python/api.py
+++ deep_learn_python/api.py
@@ -1,5 +1,34 @@
 import requests
 
-URL = 'https://developers.lingvolive.com/api/v1.1/authenticate'
-TOKEN = 'ZmViNDE5MzQtNDEyZS00NGY2LTlhY2QtZTdkNTY4MmYyY2MxOmExMmY2MGZmN2FiMjRlODRiZTRhYTIxN2RmMjI0NjZl'
+URL_AUTH = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+URL_TRANSLATE = 'https://developers.lingvolive.com/api/v1/Minicard'
+KEY = 'ZmViNDE5MzQtNDEyZS00NGY2LTlhY2QtZTdkNTY4MmYyY2MxOmExMmY2MGZmN2FiMjRlODRiZTRhYTIxN2RmMjI0NjZl'
 
+headers_auth = {'Authorization':'Basic '+KEY}
+auth = requests.post(URL_AUTH, headers=headers_auth)
+
+if auth.status_code == 200:
+    TOKEN = auth.text
+    
+    while True:
+        word = input('Input word for translate or "q" for exit: ')
+        if word == 'q':
+            exit()
+        headers_translate = {
+            'Authorization':'Bearer ' + TOKEN
+        }
+        params_translate = {
+            'text':word,
+            'srcLang':1033,
+            'dstLang':1049
+        }
+        result = requests.get(URL_TRANSLATE, headers=headers_translate, params=params_translate)
+        res = result.json()
+        try:
+            print(res['Translation']['Translation'])
+        except:
+            print('Не найдено разультатов')
+
+else:
+    print('Error')
+    exit()
