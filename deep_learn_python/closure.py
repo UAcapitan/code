0655794b658d0907b7e10ebb021a8aea502e2ae6
EdMix <ua.capitan@gmail.com>
2021-10-19 01:07:37 +0300

    DLP - closure

--- deep_learn_python/closure.py
+++ deep_learn_python/closure.py
@@ -1,26 +1,7 @@
-age = 27
-def birthday(): 
-  age = 28
-birthday()
-print(age)
+def func1(a,b):
+  def func2(c):
+    print(str(a+b+c))
+  return func2
 
-def countdown(start):
-  def display():
-    n = start
-    while n > 0:
-      n -= 1
-      print('T-minus %d' % n)
- 
-  display()
-countdown(3)
-
-def countdown(start):
-  def display():
-    n = start
-    while n > 0:
-      n -= 1
-      print('T-minus %d' % n)
-  return display
-
-counter1 = countdown(2)
-counter1()
+f1 = func1(10,20)
+f1(70)
