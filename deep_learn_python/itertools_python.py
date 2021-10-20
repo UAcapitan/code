aecd0191e309201400bac10318270ead621bac7d
EdMix <ua.capitan@gmail.com>
2021-10-21 01:01:31 +0300

    DLP - itertools: chain, combinations, c_with_re...

--- deep_learn_python/itertools_python.py
+++ deep_learn_python/itertools_python.py
@@ -9,22 +9,37 @@ for i in itertools.count(5, 5):
         pass
 
 # itertools.cycle ----------------------------------
-
 i = itertools.cycle(list('ABC'))
+
 # for x in range(10):
     # print(next(i), end='\n')
 
 # itertools.repeat ---------------------------------
-n = 5
-i = itertools.repeat(1,n)
-# for x in range(n):
-#     print(next(i))
+i = itertools.repeat(1,5)
+
+# for x in i:
+#     print(x)
 
 # itertools.accumulate -----------------------------
-
 i = itertools.accumulate([1,2,3,4,5])
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
+
+# for x in i:
+#     print(x)
+
+# itertools.chain ----------------------------------
+i = itertools.chain([1,2,3], [123,456,789], [9,8,7])
+
+# for x in i:
+#     print(x)
+
+# itertools.combinations ---------------------------
+i = itertools.combinations('ABCDF', 2)
+
+# for x in i:
+#     print('{}{}'.format(x[0], x[1]))
+
+# itertools.combinations_with_replacement ----------
+i = itertools.combinations_with_replacement('ABCDF', 2)
+
+# for x in i:
+#     print('{}{}'.format(x[0], x[1]))
