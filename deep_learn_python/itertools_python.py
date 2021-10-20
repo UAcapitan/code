f79cc59d8bdf805311762d0c23f8c6ccade74662
EdMix <ua.capitan@gmail.com>
2021-10-20 23:53:00 +0300

    DLP - itertools: cycle, repeat, accumulate

--- deep_learn_python/itertools_python.py
+++ deep_learn_python/itertools_python.py
@@ -5,4 +5,26 @@ for i in itertools.count(5, 5):
     if i == 35:
         break
     else:
-        print(i, end =" ")
+        # print(i, end =" ")
+        pass
+
+# itertools.cycle ----------------------------------
+
+i = itertools.cycle(list('ABC'))
+# for x in range(10):
+    # print(next(i), end='\n')
+
+# itertools.repeat ---------------------------------
+n = 5
+i = itertools.repeat(1,n)
+# for x in range(n):
+#     print(next(i))
+
+# itertools.accumulate -----------------------------
+
+i = itertools.accumulate([1,2,3,4,5])
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
