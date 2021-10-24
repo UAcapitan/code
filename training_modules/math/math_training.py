fb3b9f1caf8056c2d5549da43c93878bba5dd3f2
EdMix <ua.capitan@gmail.com>
2021-10-25 00:17:11 +0300

    TM - math

---
+++ training_modules/math/math_training.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.sqrt(4)) # квадратный корень
+print(math.factorial(4)) # факториал
+print(math.ceil(4.5)) # округление в большую сторону
+print(math.floor(10.2)) # окгругление в меньшую сторону
+print(math.exp(5)) # e^x
+print(math.log(10,2.3)) # логарифм
+print(math.pow(2,3)) # x^y
+print(math.sin(10)) # sin
+print(math.cos(10)) # cos
+print(math.tan(10)) # tan
+print(math.pi) # число pi
+print(math.e) # число e
