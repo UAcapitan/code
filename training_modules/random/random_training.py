5c6d4f3072e1d6cd8793f17637cfd8cc7045be85
EdMix <ua.capitan@gmail.com>
2021-10-27 00:17:13 +0300

    TM - random

---
+++ training_modules/random/random_training.py
@@ -0,0 +1,8 @@
+import random
+
+l = [1,2,3]
+print(random.choice(l)) # случайный выбор со списка
+
+print(random.randint(1,10)) # случайное целое число
+
+print(random.random()) # случайное число от 0 до 1
