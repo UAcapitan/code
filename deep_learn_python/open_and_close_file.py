a938fd2ca0e5f63d94c6d30eec0d0f2a7b902468
EdMix <ua.capitan@gmail.com>
2021-11-03 00:19:07 +0200

    DLP - open and close file with 'with'

--- deep_learn_python/open_and_close_file.py
+++ deep_learn_python/open_and_close_file.py
@@ -2,6 +2,9 @@ fo = open("foo.txt", "wb")
 print ("Name of the file: ", fo.name)
 print ("Closed or not : ", fo.closed)
 print ("Opening mode : ", fo.mode)
-
 fo.close()
 
+with open('foo.txt', 'wb') as reader:
+    print(reader.name)
+    print(reader.closed)
+    print(reader.mode)
