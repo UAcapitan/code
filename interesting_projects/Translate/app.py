7469f371e0bbefff7d0b8858260b15cd0eb3f209
EdMix <ua.capitan@gmail.com>
2021-11-04 18:13:43 +0200

    Tr - finally version of program

--- interesting_projects/Translate/app.py
+++ interesting_projects/Translate/app.py
@@ -1,6 +1,7 @@
 import googletrans
 import sys
 import PyQt5.QtWidgets as qt5
+import webbrowser
 
 translator = googletrans.Translator()
 
@@ -116,41 +117,99 @@ class TranslateApp(qt5.QMainWindow):
         self.nw_save_text.show()
 
     def save_all_text(self):
-        self.nw_save_all_text = AnotherWindow('Save all text in file', [400, 400])
+        self.nw_save_all_text = AnotherWindow('Save text in file', [400, 115])
+        self.nw_save_all_text.label_address = qt5.QLabel('Address for save file', self.nw_save_all_text)
+        self.nw_save_all_text.label_address.move(10,10)
+        self.nw_save_all_text.text_address = qt5.QPlainTextEdit(self.nw_save_all_text)
+        self.nw_save_all_text.text_address.insertPlainText('D://text_all.txt')
+        self.nw_save_all_text.text_address.move(10,30)
+        self.nw_save_all_text.text_address.resize(380,30)
+        self.nw_save_all_text.button_save = qt5.QPushButton('Save', self.nw_save_all_text)
+        self.nw_save_all_text.button_save.move(250,70)
+        self.nw_save_all_text.button_save.clicked.connect(self.save_all_in_file_button)
         self.nw_save_all_text.show()
 
     def all_languages(self):
-        self.nw_all_lang = AnotherWindow('All languages', [400, 400])
+        self.nw_all_lang = AnotherWindow('All languages', [200, 400])
+        
+        all_lang = ''
+        for k, v in googletrans.LANGUAGES.items():
+            all_lang += k + ' - ' + v + '\n'
+
+        self.nw_all_lang.all_lang = qt5.QPlainTextEdit(self.nw_all_lang)
+        self.nw_all_lang.all_lang.insertPlainText(all_lang)
+        self.nw_all_lang.all_lang.move(10,10)
+        self.nw_all_lang.all_lang.resize(180,380)
+        self.nw_all_lang.all_lang.setReadOnly(True)
         self.nw_all_lang.show()
 
     def about_version(self):
-        self.nw_about_version = AnotherWindow('About this version', [400, 150])
+        self.nw_about_version = AnotherWindow('About this version', [400, 60])
+        self.nw_about_version.label_text = qt5.QLabel('Version', self.nw_about_version)
+        self.nw_about_version.label_text.move(10,10)
+        self.nw_about_version.label_version = qt5.QLabel('1.00', self.nw_about_version)
+        self.nw_about_version.label_version.move(10,30)
         self.nw_about_version.show()
 
     def open_site(self):
-        self.nw_open_site = AnotherWindow('Open site', [400, 150])
+        self.nw_open_site = AnotherWindow('Open site', [400, 70])
+        self.nw_open_site.label_text = qt5.QLabel('Official site', self.nw_open_site)
+        self.nw_open_site.label_text.move(10,10)
+        self.nw_open_site.button_open = qt5.QPushButton('Open', self.nw_open_site)
+        self.nw_open_site.button_open.move(10,30)
+        self.nw_open_site.button_open.clicked.connect(self.open_official_site)
         self.nw_open_site.show()
 
     def what_is_new(self):
-        self.nw_what_is_new = AnotherWindow('What`s new?', [400, 200])
+        self.nw_what_is_new = AnotherWindow('What`s new?', [400, 80])
+        self.nw_what_is_new.label_text = qt5.QLabel('New Updates', self.nw_what_is_new)
+        self.nw_what_is_new.label_text.move(10,10)
+        self.nw_what_is_new.label_updates = qt5.QLabel('- Working menu windows\n- Saving translations with the original text', self.nw_what_is_new)
+        self.nw_what_is_new.label_updates.move(10,30)
         self.nw_what_is_new.show()
 
     def support(self):
-        self.nw_support = AnotherWindow('Support', [400, 250])
+        self.nw_support = AnotherWindow('Support', [400, 70])
+        self.nw_support.label_text = qt5.QLabel('Support mail:', self.nw_support)
+        self.nw_support.label_text.move(10,10)
+        self.nw_support.all_lang = qt5.QPlainTextEdit(self.nw_support)
+        self.nw_support.all_lang.insertPlainText('ua.capitan@gmail.com')
+        self.nw_support.all_lang.move(10,30)
+        self.nw_support.all_lang.resize(380,30)
+        self.nw_support.all_lang.setReadOnly(True)
         self.nw_support.show()
 
     def about_program(self):
-        self.nw_about_program = AnotherWindow('About program', [400, 200])
+        self.nw_about_program = AnotherWindow('About program', [400, 115])
+        self.nw_about_program.label_text = qt5.QLabel('About program', self.nw_about_program)
+        self.nw_about_program.label_text.move(10,10)
+        self.nw_about_program.label_about = qt5.QLabel('This is program for translating text. Also\nyou can save translated text in file.\nYou can see all possible language\nin menu item `All languages`.', self.nw_about_program)
+        self.nw_about_program.label_about.move(10,30)
         self.nw_about_program.show()
 
     def about_author(self):
-        self.nw_about_author = AnotherWindow('About author', [400, 200])
+        self.nw_about_author = AnotherWindow('About author', [400, 80])
+        self.nw_about_author.label_text = qt5.QLabel('About author', self.nw_about_author)
+        self.nw_about_author.label_text.move(10,10)
+        self.nw_about_author.label_author = qt5.QLabel('I am Ed. I am Python developer. You can\nsee my portfolio in menu item `Official site`.', self.nw_about_author)
+        self.nw_about_author.label_author.move(10,30)
         self.nw_about_author.show()
 
     # Function for save in files
     def save_in_file_button(self):
         with open(self.nw_save_text.text_address.toPlainText(), 'a') as f:
             f.write(self.text_translated.toPlainText() + '\n\n')
+        self.nw_save_text.hide()
+
+    def save_all_in_file_button(self):
+        with open(self.nw_save_all_text.text_address.toPlainText(), 'a') as f:
+            f.write(self.text_for_translate.toPlainText() + '\n\n')
+            f.write(self.text_translated.toPlainText() + '\n\n-------------------------------------------\n\n')
+        self.nw_save_all_text.hide()
+
+    # Open official site
+    def open_official_site(self):
+        webbrowser.open('http://edmix.herokuapp.com/')
 
     # Function exit
     def exit(self,app):
