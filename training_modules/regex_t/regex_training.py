8b0ab57de9a8f29fdea6b7ac1292ed45a5264dc9
EdMix <ua.capitan@gmail.com>
2021-11-07 00:32:32 +0200

    TM - regex

--- training_modules/regex_t/regex_training.py
+++ training_modules/regex_t/regex_training.py
@@ -1,12 +1,18 @@
 import re
 
-# pattern_1 = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
-# pattern_2 = "(\d\d\d)-(\d\d\d)-(\d\d\d\d)"
-pattern_3 = '[a-z]-[A-Z]-[0-9]'
+# pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
+# pattern = "(\d\d\d)-(\d\d\d)-(\d\d\d\d)"
+# pattern = '[a-z]-[A-Z]-[0-9]'
+# pattern = '[\d]{10}-[abc]v1'
+# pattern = '\t\s\t\s\w'
+# pattern = '[^a]\d\t\w'
+# pattern = '.{3}-\w'
+# pattern = '^[a-zA-Z0-9]{5}$'
+pattern = '(good|bad|nice) man'
 
 user_input = input()
 
-if (re.search(pattern_3, user_input)):
+if (re.search(pattern, user_input)):
     print('Valid')
 else:
     print('Not valid')
