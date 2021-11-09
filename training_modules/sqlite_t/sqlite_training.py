69ba2ccc938d9c2ba28fd5e2efea4a041dc67503
EdMix <ua.capitan@gmail.com>
2021-11-09 12:14:11 +0200

    TM - sqlite

--- training_modules/sqlite_t/sqlite_training.py
+++ training_modules/sqlite_t/sqlite_training.py
@@ -1,4 +1,8 @@
 import sqlite3
 
-db = sqlite3.connect('database.db')
-db.close()
+with sqlite3.connect('database.db') as db:
+    cursor = db.cursor()
+    # query = """ CREATE TABLE IF NOT EXISTS users(id INTEGER, name TEXT, login TEXT, password TEXT) """
+    query = """INSERT INTO users(id, name, login, password) VALUES ('1', 'Ed', 'EdMix', 'edmix22')"""
+    cursor.execute(query)
+    db.commit()
