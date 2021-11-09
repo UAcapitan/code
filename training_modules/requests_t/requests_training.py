014a41208e7c7754c302f8e337433da153884638
EdMix <ua.capitan@gmail.com>
2021-11-10 00:57:18 +0200

    TM - requests

--- training_modules/requests_t/requests_training.py
+++ training_modules/requests_t/requests_training.py
@@ -21,3 +21,20 @@ response = requests.post('https://httpbin.org/post', data=[('key','value')])
 print(response.status_code, end='\n\n')
 print(response.url, end='\n\n')
 print(response.request.body, end='\n\n')
+
+response = requests.get('https://httpbin.org/get')
+
+print(response.status_code, end='\n\n')
+print(response.text, end='\n\n')
+print(response.json(), end='\n\n')
+
+url = 'https://httpbin.org/post'
+query = [('search_car','kia')]
+
+response = requests.post(url, data=query)
+response = requests.put(url, data=query)
+response = requests.patch(url, data=query)
+response = requests.delete(url)
+response = requests.head(url)
+response = requests.options(url)
+
