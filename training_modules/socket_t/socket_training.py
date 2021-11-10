83ee81f3cbbd8836e7181f9beff71a293368ac0e
EdMix <ua.capitan@gmail.com>
2021-11-10 17:13:40 +0200

    TM - socket

--- training_modules/socket_t/socket_training.py
+++ training_modules/socket_t/socket_training.py
@@ -3,15 +3,32 @@ import socket
 # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # server.bind(('127.0.0.1', 2000))
 
-server = socket.create_server(('127.0.0.1', 2000))
-server.listen(4)
+class Server:
+    def __init__(self):
+        self.server = socket.create_server(('127.0.0.1', 2000))
+        self.server.listen(4)
+        self.HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode('utf-8')
 
-while True:
-    print('Working')
-    client_socket, address = server.accept()
-    data = client_socket.recv(1024).decode('utf-8')
-    print(data)
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-    text = 'Hello, world'.encode('utf-8')
-    client_socket.send(HDRS.encode('utf-8') + text)
-    print('Final')
+    def run(self):
+        try:
+            while True:
+                self.client_socket, self.address = self.server.accept()
+                data = self.client_socket.recv(1024).decode('utf-8')
+                text = self.load_file(data)
+                self.client_socket.send(text)
+                self.client_socket.shutdown(socket.SHUT_WR)
+        except:
+            print('Error')
+            self.server.close()
+
+    def load_file(self, data):
+        path = data.split()[1]
+        result = ''
+        with open('views' + path, 'rb') as file:
+            result = file.read()
+        print(self.HDRS + result)
+        return self.HDRS + result
+            
+if __name__ == '__main__':
+    server = Server()
+    server.run()
