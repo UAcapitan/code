5e9596fcc8e7c31b52e5ac76558c706a363ecf20
EdMix <ua.capitan@gmail.com>
2021-11-14 15:23:31 +0200

    TF - DJ: create ORM model

--- training_frameworks/django_t/trainingsite/trainingsite/settings.py
+++ training_frameworks/django_t/trainingsite/trainingsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#(3+zauu$v_ncmte$-6m%^*_wl#r5kj_0p8qf0t1*lw^xe9w='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- training_frameworks/django_t/trainingsite/trainingsite/urls.py
+++ training_frameworks/django_t/trainingsite/trainingsite/urls.py
@@ -17,10 +17,15 @@ from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 from appmain.views import pageNotFound
+from django.conf.urls.static import static
+from trainingsite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appmain.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
