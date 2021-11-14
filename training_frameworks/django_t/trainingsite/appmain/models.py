a27aaeefd14cd78b09951711b8a00cc5657e6f5f
EdMix <ua.capitan@gmail.com>
2021-11-15 01:52:19 +0200

    TF - DJ: admin

--- training_frameworks/django_t/trainingsite/appmain/models.py
+++ training_frameworks/django_t/trainingsite/appmain/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Name')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     time_create = models.DateTimeField(auto_now_add=True)
@@ -15,6 +15,11 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article', kwargs={'id': self.pk})
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ['-title', 'id']
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
