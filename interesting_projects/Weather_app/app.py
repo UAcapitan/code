1f4bcb57b1ed07bde42065ceb48618747feb4d93
EdMix <ua.capitan@gmail.com>
2021-11-15 02:40:51 +0200

    WA - it show weather

--- interesting_projects/Weather_app/app.py
+++ interesting_projects/Weather_app/app.py
@@ -1,6 +1,18 @@
 import tkinter as tk
+import pyowm
 
 root = tk.Tk()
+owm = pyowm.OWM('ba5efb246df12da53dec4af7c4583ce7')
+mgr = owm.weather_manager()
+daily_forecast = mgr.forecast_at_place('Florida', '3h').forecast
+t_3h = 0
+l = 0
+for weather in daily_forecast:
+    # print(weather.reference_time('iso'), weather.temperature(unit='celsius')['temp'])
+    t_3h += weather.temperature(unit='celsius')['temp']
+    l += 1
+
+t = t_3h / l
+print(t)
 
 root.mainloop()
-
