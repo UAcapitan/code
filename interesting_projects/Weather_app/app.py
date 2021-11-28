ce19559d7ffc07fd11c74abe3c706b11a556378e
EdMix <ua.capitan@gmail.com>
2021-11-28 03:24:55 +0200

    WA - add new windows

--- interesting_projects/Weather_app/app.py
+++ interesting_projects/Weather_app/app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import pyowm
 import datetime
 import sys
+import json
 
 class App():
     def __init__(self, root, mgr):
@@ -16,7 +17,7 @@ class App():
         self.text.place(x=120, y=150)
 
         self.day_av = tk.Label(self.root, text='Day average', font=("Arial", 10))
-        self.day_av.place(x=90, y=200)
+        self.day_av.place(x=88, y=200)
 
         self.input_text = tk.Text(self.root, height=1, width=10)
         self.input_text.place(x=88, y=320)
@@ -31,11 +32,11 @@ class App():
         self.night = tk.Label(self.root, text='-', font=("Arial", 15))
         self.night.place(x=50, y=240)
 
-        self.day_text = tk.Label(self.root, text='Day', font=("Arial", 10))
-        self.day_text.place(x=175, y=270)
+        self.day_text = tk.Label(self.root, text='Next day', font=("Arial", 10))
+        self.day_text.place(x=162, y=270)
 
-        self.night_text = tk.Label(self.root, text='Night', font=("Arial", 10))
-        self.night_text.place(x=40, y=270)
+        self.night_text = tk.Label(self.root, text='Next night', font=("Arial", 10))
+        self.night_text.place(x=26, y=270)
 
         self.canvas = tk.Canvas(root, width=64, height=64)  
         self.canvas.place(x=93, y=50)    
@@ -43,9 +44,9 @@ class App():
         self.main_menu = tk.Menu(root) 
         self.root.config(menu=self.main_menu)
         self.extra_menu = tk.Menu(self.main_menu, tearoff=0)
-        self.extra_menu.add_command(label="Days")
+        self.extra_menu.add_command(label="Days", command=self.show_weather_days)
         self.settings_menu = tk.Menu(self.main_menu, tearoff=0)
-        self.settings_menu.add_command(label="Main city")
+        self.settings_menu.add_command(label="Main city", command=self.show_main_city)
         self.main_menu.add_cascade(label="Extra", menu=self.extra_menu)
         self.main_menu.add_cascade(label="Settings", menu=self.settings_menu)
         self.main_menu.add_command(label='Exit', command=self.app_exit)
@@ -65,8 +66,11 @@ class App():
                     t_3h += weather.temperature(unit='celsius')['temp']
                     l += 1
 
-            self.text.place(x=90, y=150)
-            self.text['text'] = str(round(t_3h/l)) + ' °C'
+            try:
+                self.text.place(x=90, y=150)
+                self.text['text'] = str(round(t_3h/l)) + ' °C'
+            except:
+                self.text['text'] = '-'
 
             t_3h = 0
             l = 0
@@ -84,7 +88,7 @@ class App():
             l = 0
 
             for weather in daily_forecast:
-                if int(weather.reference_time('iso')[8:10]) == self.dt.day:
+                if int(weather.reference_time('iso')[8:10]) == self.dt.day + 1:
                     if 12 <= int(weather.reference_time('iso')[11:13]) <= 18:
                         t_3h += weather.temperature(unit='celsius')['temp']
                         l += 1
@@ -129,6 +133,20 @@ class App():
         self.label_error = tk.Label(self.error_window, text = "Error")
 
         self.label_error.pack()
+
+    def show_main_city(self):
+        self.main_city = tk.Toplevel(self.root)
+        self.main_city.geometry("200x70")
+        self.main_city_entry = tk.Entry(self.main_city)
+        self.main_city_btn = tk.Button(self.main_city, text="Save")
+
+        self.main_city_entry.place(x=40, y=10)
+        self.main_city_btn.place(x=83, y=40)
+    
+    def show_weather_days(self):
+        self.weather_days = tk.Toplevel(self.root)
+        self.weather_days.geometry("250x400")
+        self.weather_days.title("List of weather")
 
 
 if __name__ == '__main__':
