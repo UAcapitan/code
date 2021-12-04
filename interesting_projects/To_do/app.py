3bfc1f85a8ff303a8698dada4eb865dbefb542f2
EdMix <ua.capitan@gmail.com>
2021-12-05 00:35:19 +0200

    TD - templates and model

--- interesting_projects/To_do/app.py
+++ interesting_projects/To_do/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/to_do.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return self.id
 
 @app.route('/')
 def main_page():
-    return '<h1>To do</h1>'
+    db.session.add(Article(text="New test text"))
+    db.session.commit()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
