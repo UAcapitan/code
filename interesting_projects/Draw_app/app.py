26578e7ba295d43ee9c653e87ab1c29698bb75f7
EdMix <ua.capitan@gmail.com>
2021-12-16 00:57:27 +0200

    Draw - update size of window

--- interesting_projects/Draw_app/app.py
+++ interesting_projects/Draw_app/app.py
@@ -1,45 +1,50 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.geometry("300x370")
-root.title("Paint app")
+class DrawApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("700x370")
+        self.root.title("Paint app")
 
-color = '#00FF00'
-draw = False
+        self.color = '#00FF00'
+        self.draw = False
 
-cnv = tk.Canvas(root, bg="white", height=300, width=300)
-cnv.place(x=0, y=0)
-f1 = tk.Text(root, height=1, width=10)
-f1.place(x=90, y=310)
+        self.cnv = tk.Canvas(self.root, bg="white", height=300, width=700)
+        self.cnv.place(x=0, y=0)
+        self.f1 = tk.Text(self.root, height=1, width=10)
+        self.f1.place(x=90, y=310)
 
-def change_color():
-    global color
-    color = f1.get('1.0', tk.END)[0:7]
-    cnv_color.create_rectangle(0, 0, 20, 20, fill=color, outline='')
-    print(color)
+        self.f_btn = tk.Button(text='Change', command=self.change_color)
+        self.f_btn.place(x=190, y=307)
 
-f_btn = tk.Button(text='Change', command=change_color)
-f_btn.place(x=190, y=307)
+        self.cnv_color = tk.Canvas(self.root, bg="white", height=20, width=20)
+        self.cnv_color.place(x=50, y=308)
+        self.cnv_color.create_rectangle(0, 0, 20, 20, fill=self.color, outline='')
 
-def motion(event):
-    if draw:
-        x, y = event.x, event.y
-        print('{}, {}'.format(x, y))
-        cnv.create_rectangle(x, y, x+5, y+5, fill=color, outline='')
+        self.root.bind('<Motion>', self.motion)
+        self.root.bind('q', self.draw_flag)
 
-def draw_flag(event):
-    global draw
-    if draw:
-        draw = False
-    else:
-        draw = True
-    print(draw)
+    def change_color(self):
+        color = self.f1.get('1.0', tk.END)[0:7]
+        self.cnv_color.create_rectangle(0, 0, 20, 20, fill=color, outline='')
+        print(color)
 
-cnv_color = tk.Canvas(root, bg="white", height=20, width=20)
-cnv_color.place(x=50, y=308)
-cnv_color.create_rectangle(0, 0, 20, 20, fill=color, outline='')
+    def motion(self, event):
+        if self.draw:
+            x, y = event.x, event.y
+            print('{}, {}'.format(x, y))
+            self.cnv.create_rectangle(x, y, x+5, y+5, fill=self.color, outline='')
 
-root.bind('<Motion>', motion)
-root.bind('q', draw_flag)
+    def draw_flag(self, event):
+        if self.draw:
+            self.draw = False
+        else:
+            self.draw = True
+        print(self.draw)
 
-root.mainloop()
+    def app_run(self):
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    app = DrawApp()
+    app.app_run()
