669d27f5f35ece8eb52c2b09314188a1c2771d56
EdMix <ua.capitan@gmail.com>
2021-12-25 22:15:12 +0200

    TSA - finally version program

--- interesting_projects/Typing_speed_app/app.py
+++ interesting_projects/Typing_speed_app/app.py
@@ -13,9 +13,7 @@ class TSApp:
         self.root.title('Typing speed app')
         self.root.geometry('700x300+100+100')
         self.start()
-
-        self.root.bind('<Key>', self.key_pressed)
-        self.root.bind('<KeyPress-Shift_L>', self.shift_delete)
+        self.set_binds()
 
     def set_variables(self):
         self.right_answers = 0
@@ -43,8 +41,20 @@ class TSApp:
         self.wrong_label = tk.Label(self.root, text='Wrong: ' + str(self.wrong_answers), fg='#FF0000')
         self.right_label.place(x=10, y=260)
         self.wrong_label.place(x=10, y=280)
-        self.letter_label = tk.Label(self.root, text='L', bg='#00FF00', width=5, height=2)
+        self.letter_label = tk.Label(self.root, text='...', bg='#00FF00', width=5, height=2)
         self.letter_label.place(x=650, y=260)
+
+    def set_binds(self):
+        self.root.bind('<Key>', self.key_pressed)
+
+        self.set_wrong_binds()
+
+    def set_wrong_binds(self):
+        self.root.bind('<Shift_L>', self.wrong_delete)
+        self.root.bind('<Shift_R>', self.wrong_delete)
+        self.root.bind('<Caps_Lock>', self.wrong_delete)
+        self.root.bind('<Control_L>', self.wrong_delete)
+        self.root.bind('<Control_R>', self.wrong_delete)
 
     def open_list_of_texts_json(self):
         with open('src/texts.json', 'r') as file:
@@ -82,7 +92,7 @@ class TSApp:
             self.set_text_in_main_field()
             self.check_end()
 
-    def shift_delete(self, event):
+    def wrong_delete(self, event):
         if self.wrong_answers != 0:
             self.wrong_answers -= 1
 
@@ -113,6 +123,8 @@ class TSApp:
         self.symbols_end_label = tk.Label(self.end_window, text='Symbols in one minute: ' + self.symbols_per_minute())
         self.symbols_end_label.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
+        self.save_result_in_json()
+
     def count_time(self):
         self.time_result = int(self.time_end) - int(self.time_begin)
 
@@ -120,6 +132,8 @@ class TSApp:
         return str(datetime.timedelta(seconds = self.time_result))
 
     def symbols_per_minute(self):
+        if int(self.time_result) == 0:
+            self.time_result = 1
         return str(int(self.right_answers / int(self.time_result) * 60))
 
     def start(self):
@@ -140,12 +154,54 @@ class TSApp:
     def open_add_window(self):
         self.add_window = tk.Toplevel(self.root)
         self.add_window.title('Add')
-        self.add_window.geometry('400x150+150+150')
+        self.add_window.geometry('200x120+150+150')
+
+        self.text_add_label = tk.Label(self.add_window, text='Add new text')
+        self.field_add_entry = tk.Entry(self.add_window)
+        self.save_add_button = tk.Button(self.add_window, text='Save', command=self.save_new_text_in_json)
+
+        self.text_add_label.place(x=20, y=20)
+        self.field_add_entry.place(x=20, y=50)
+        self.save_add_button.place(x=20, y=80)
 
     def open_results_window(self):
         self.results_window = tk.Toplevel(self.root)
         self.results_window.title('Results')
-        self.results_window.geometry('400x150+150+150')
+        self.results_window.geometry('250x450+150+150')
+
+        tk.Label(self.results_window, text='Id   Date   Time   Right   Per/Min').place(relx=0.5, rely=0.05, anchor=tk.CENTER)
+
+        list_of_results = self.open_results_in_json()[:9]
+        y = 0.1
+        for i in list_of_results:
+            tk.Label(self.results_window, text=i).place(relx=0.5, rely=y, anchor=tk.CENTER)
+            y += 0.1
+
+    def save_new_text_in_json(self):
+        texts = {
+            "texts":self.open_list_of_texts_json()
+        }
+        texts['texts'].append(self.field_add_entry.get())
+        with open('src/texts.json', 'w') as file:
+            json.dump(texts, file)
+        self.field_add_entry.delete(0,tk.END)
+        self.add_window.destroy()
+
+    def open_results_in_json(self):
+        with open('src/results.json', 'r') as file:
+            return list(reversed(json.load(file)['results']))
+
+    def save_result_in_json(self):
+        list_of_results = list(reversed(self.open_results_in_json()))
+        id_article = str(len(list_of_results)+1)
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        list_of_results.append(id_article + ' ' + date + ' ' + self.convert_time() 
+        + ' ' + str(self.right_answers) + ' ' + self.symbols_per_minute())
+        results = {
+            "results":list_of_results,
+        }
+        with open('src/results.json', 'w') as file:
+            json.dump(results, file)
 
     def run(self):
         self.root.mainloop()
@@ -153,4 +209,5 @@ class TSApp:
 if __name__ == '__main__':
     app = TSApp()
     # app.open_end_window()
+    # app.save_result_in_json()
     app.run()
