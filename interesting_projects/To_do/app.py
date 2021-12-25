becc63efda272b1fff6e02f7dd8e5a6a8884a810
EdMix <ua.capitan@gmail.com>
2021-12-25 22:52:12 +0200

    TD - fix some things

--- interesting_projects/To_do/app.py
+++ interesting_projects/To_do/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import desc
-from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/to_do.db'
