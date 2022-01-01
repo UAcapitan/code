1a2031f4c9022ca48f8d3f74b55bef48d41127da
EdMix <ua.capitan@gmail.com>
2022-01-01 03:21:32 +0200

    GB - add and show articles

--- interesting_projects/Game_blog/gameblog/appmain/forms.py
+++ interesting_projects/Game_blog/gameblog/appmain/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from . import models
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
@@ -16,6 +17,15 @@ class NewUserForm(UserCreationForm):
 			user.save()
 		return user 
 
-class ArticleForm(forms.Form):
-	title = forms.CharField(label='Title', max_length=100)
-	text = forms.CharField(widget=forms.Textarea)
+# class ArticleForm(forms.Form):
+# 	title = forms.CharField(label='Title', max_length=100)
+# 	text = forms.CharField(label='Text', max_length=2560)
+
+class ArticleForm(forms.ModelForm):
+	title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Title'}))
+	text = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Text'}), max_length=2560)
+	date_of_save = forms.DateField(input_formats=['%d-%m-%Y'], widget=forms.DateInput(attrs={'placeholder':'DD-MM-YYYY'}))
+
+	class Meta:
+		model = models.Article
+		fields = ['title', 'text', 'date_of_save']
--- interesting_projects/Game_blog/gameblog/appmain/models.py
+++ interesting_projects/Game_blog/gameblog/appmain/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.forms import widgets
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     date_of_save = models.DateField()
+
+    def __str__(self):
+        return self.title
