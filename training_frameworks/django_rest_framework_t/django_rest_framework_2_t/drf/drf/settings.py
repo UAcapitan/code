59f0730fa7e2f794c92bdb082109116fb0cfe1a3
EdMix <ua.capitan@gmail.com>
2022-01-14 10:54:24 +0200

    DRF2T - corsheaders

--- training_frameworks/django_rest_framework_t/django_rest_framework_2_t/drf/drf/settings.py
+++ training_frameworks/django_rest_framework_t/django_rest_framework_2_t/drf/drf/settings.py
@@ -34,11 +34,14 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'social_django',
     'rest_framework_social_oauth2',
+
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -263,3 +266,8 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8000'
+]
