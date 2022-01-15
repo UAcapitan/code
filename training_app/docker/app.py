f6dbf42a2e5495f49365950d7014e70619a3bf3a
EdMix <ua.capitan@gmail.com>
2022-01-16 00:27:27 +0200

    Doc - add another page and install sql module

--- training_app/docker/app.py
+++ training_app/docker/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route('/next')
+def another_page():
+    return 'Another page'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
