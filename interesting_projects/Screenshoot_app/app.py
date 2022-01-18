682351a79b79d1f2bc911c8410801474237ad348
EdMix <ua.capitan@gmail.com>
2022-01-19 00:06:52 +0200

    Screen - venv and other

--- interesting_projects/Screenshoot_app/app.py
+++ interesting_projects/Screenshoot_app/app.py
@@ -1,12 +1,26 @@
 import tkinter as tk
+from PIL import ImageGrab
+import pyautogui
+
 
 class ScreenApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title('Screenshoot')
-        self.root.geometry("200x300")
+        self.root.geometry("180x120")
 
-        self.btn = tk.Button(text='Screenshoot')
+        self.entry = tk.Entry(self.root, width=15)
+        self.entry.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+
+        self.btn = tk.Button(self.root, text='Screenshoot', command=self.screen)
+        self.btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+
+    def screen(self):
+        width, height = pyautogui.size()
+        ImageGrab.grab().crop((0,0,width,height)).save('images/test/' + 'test' + '.jpg')
+
+    def number_check(self):
+        return
     
     def run(self):
         self.root.mainloop()
