8a08fa16d9031e31b479c3eca8ae557ba30601f3
EdMix <ua.capitan@gmail.com>
2022-01-20 00:10:48 +0200

    Screen - last image index

--- interesting_projects/Screenshoot_app/app.py
+++ interesting_projects/Screenshoot_app/app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import ImageGrab
 import pyautogui
+import os
 
 
 class ScreenApp:
@@ -15,16 +16,23 @@ class ScreenApp:
         self.btn = tk.Button(self.root, text='Screenshoot', command=self.screen)
         self.btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
+        self.run()
+
     def screen(self):
         width, height = pyautogui.size()
-        ImageGrab.grab().crop((0,0,width,height)).save('images/test/' + 'test' + '.jpg')
+        print(self.last_image_index())
+        ImageGrab.grab().crop((0,0,width,height)).save('images/test/' + str(self.last_image_index()) + '.jpg')
 
-    def number_check(self):
-        return
+    def last_image_index(self):
+        last = os.listdir(os.getcwd() + '/images/' + self.entry.get() + '/')
+        last = [int(i) for i in [i.replace('.jpg', '') for i in last] if i.isdigit()]
+        if last == []:
+            return 1
+        return max(last) + 1
     
     def run(self):
         self.root.mainloop()
 
+
 if __name__ == '__main__':
     app = ScreenApp()
-    app.run()
