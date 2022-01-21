5cd1a26ec7f7c060f5cb9258c469b6279bff4fae
EdMix <ua.capitan@gmail.com>
2022-01-21 17:23:18 +0200

    Screen - final version of program

--- interesting_projects/Screenshoot_app/app.py
+++ interesting_projects/Screenshoot_app/app.py
@@ -9,27 +9,23 @@ class ScreenApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title('Screenshoot')
-        self.root.geometry("300x120")
 
         self.entry = tk.Entry(self.root, width=15)
-        self.entry.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
 
         self.btn = tk.Button(self.root, text='Screenshoot', command=self.screen)
-        self.btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-        self.worked = False
+        self.set_start()
 
         self.key_down()
 
         self.run()
 
     def screen(self):
+        self.check_worked()
         width, height = pyautogui.size()
-        ImageGrab.grab().crop((0,0,width,height)).save('images/test/' + str(self.last_image_index()) + '.jpg')
+        ImageGrab.grab().crop((0,0,width,height)).save('images/' + self.entry.get() + '/' + str(self.last_image_index()) + '.jpg')
 
     def last_image_index(self):
-        self.entry.config(state='disable')
-        self.root.geometry("300x150")
         self.check_folder()
         last = os.listdir(os.getcwd() + '/images/' + self.entry.get() + '/')
         last = [int(i) for i in [i.replace('.jpg', '') for i in last] if i.isdigit()]
@@ -43,6 +39,31 @@ class ScreenApp:
 
     def key_down(self):
         keyboard.add_hotkey('o+p', self.screen)
+
+    def check_worked(self):
+        if not self.worked:
+            self.set_after_screen()
+
+    def restart(self):
+        self.entry.config(state='normal')
+        self.set_start()
+        self.entry.delete(0, 'end')
+        self.btn_restart.place_forget()
+
+    def set_start(self):
+        self.root.geometry("300x120")
+        self.entry.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        self.btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+        self.worked = False
+
+    def set_after_screen(self):
+        self.entry.config(state='disable')
+        self.root.geometry("300x190")
+        self.worked = True
+        self.btn_restart = tk.Button(text='Restart', command=self.restart)
+        self.entry.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+        self.btn.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.btn_restart.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
     
     def run(self):
         self.root.mainloop()
