ca5c859315709d9ce12736b50470b9af4d9d0a99
EdMix <ua.capitan@gmail.com>
2022-01-22 00:01:33 +0200

    PAB - create game window and some settings

--- interesting_projects/Plates_and_ball/app.py
+++ interesting_projects/Plates_and_ball/app.py
@@ -1,12 +1,38 @@
 import pygame as pg
-pg.init()
 
 COLORS = {
     'white': (255,255,255),
     'black': (0,0,0)
 }
+SIZE_ROOT = {
+    'x':700,
+    'y':400
+}
 
 class Game:
     def __init__(self):
-        self.root = pg.display.set_mode(700,500)
-        
+        pg.init()
+        pg.display.set_caption('Plates and Ball')
+        self.root = pg.display.set_mode((SIZE_ROOT['x'],SIZE_ROOT['y']))
+        self.clock = pg.time.Clock()
+        self.work = True
+
+        self.run()
+
+    def run(self):
+        while self.work:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.work = False
+            self.clock.tick(60)
+            pg.display.flip()
+
+        pg.quit()
+
+    def bg_screen(self):
+        self.root.fill(COLORS['black'])
+        pg.draw.rect(self.root, COLORS['white'], [SIZE_ROOT['x']/2, 0, SIZE_ROOT['x']/2, SIZE_ROOT['y']])
+
+
+if __name__ == '__main__':
+    game = Game()
