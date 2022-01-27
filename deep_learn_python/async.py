2a48a445aab9c7d9378663e889758269af7c4b8b
EdMix <ua.capitan@gmail.com>
2022-01-27 11:39:28 +0200

    AB - first lesson code

--- deep_learn_python/async.py
+++ deep_learn_python/async.py
@@ -4,3 +4,22 @@ socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 socket_server.bind(('localhost',5000))
 socket_server.listen()
+
+while True:
+    socket_client, addr = socket_server.accept()
+    print('Connection from', addr)
+
+    while True:
+        print('Input data')
+        request = socket_client.recv(4096)
+        print('Working')
+
+        if not request:
+            print('Error')
+            break
+        else:
+            response = 'Hello, world\n'.encode()
+            socket_client.send(response)
+            
+        print('Exit from server')
+        socket_client.close()
