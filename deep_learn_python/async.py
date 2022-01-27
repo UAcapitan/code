67916baad08734162c30973c9fb3088504f52039
EdMix <ua.capitan@gmail.com>
2022-01-27 21:28:32 +0200

    AB - async with event loop

--- deep_learn_python/async.py
+++ deep_learn_python/async.py
@@ -1,25 +1,38 @@
 import socket
+from select import select
+
+sockets = []
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 socket_server.bind(('localhost',5000))
 socket_server.listen()
 
-while True:
+def accept_connection(socket_server):
     socket_client, addr = socket_server.accept()
     print('Connection from', addr)
+    sockets.append(socket_client)
 
-    while True:
-        print('Input data')
-        request = socket_client.recv(4096)
-        print('Working')
+def send_message(socket_client):
+    request = socket_client.recv(4096)
 
-        if not request:
-            print('Error')
-            break
-        else:
-            response = 'Hello, world\n'.encode()
-            socket_client.send(response)
-            
-        print('Exit from server')
+    if request:
+        response = 'Hello, world\n'.encode()
+        socket_client.send(response)
+    else:    
+        sockets.remove(socket_client)    
         socket_client.close()
+
+def event_loop():
+    while True:
+        read_sockets, _, _ = select(sockets, [], [])
+
+        for sock in read_sockets:
+            if sock is socket_server:
+                accept_connection(sock)
+            else:
+                send_message(sock)
+
+if __name__ == '__main__':
+    sockets.append(socket_server)
+    event_loop()
