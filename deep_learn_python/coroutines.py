0abbecc8e6f6d53c835fc4e88525391a2e5b2145
EdMix <ua.capitan@gmail.com>
2022-01-28 20:01:47 +0200

    AB - coroutines error except

--- deep_learn_python/coroutines.py
+++ deep_learn_python/coroutines.py
@@ -1,13 +1,31 @@
+def coroutine(func):
+    def inner():
+        g = func()
+        g.send(None)
+        return g
+    return inner
+
+@coroutine
 def gen():
-    try:
+        num = 5
+
         while num < 25:
-            num = 5
-            print('Number:',num)
-            x = yield num
-    except StopIteration:
-        print('End of work')
-    else:
-        print('X:', x)
-        num += x
+            try:
+                x = yield 'Number ' + str(num)
+            except StopIteration:
+                break
+            else:
+                num += x
+        
+        return num
 
 g = gen()
+try:  
+    print(g.send(5))
+    print(g.send(5))
+    print(g.send(5))
+    print(g.send(5))
+    print(g.send(5))  
+    # print(g.throw(StopIteration))
+except StopIteration as e:
+    print('Result:', e.value)
