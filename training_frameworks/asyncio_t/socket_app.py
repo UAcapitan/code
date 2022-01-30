f24dcf9716f83e7ac1c2f8e940512fe842327669
EdMix <ua.capitan@gmail.com>
2022-01-31 00:03:04 +0200

    AB - attempt to server

--- training_frameworks/asyncio_t/socket_app.py
+++ training_frameworks/asyncio_t/socket_app.py
@@ -1,20 +1,18 @@
 import socket
 import asyncio
 
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_server.bind(('localhost',5000))
-socket_server.listen()
-clients = []
-
 async def accept_connection(socket_server):
+    print('Server is listening')
+    loop = asyncio.get_event_loop()
     global clients
     while True:
         socket_client, addr = socket_server.accept()
         print('Connection from', addr)
-        clients.append(socket_client)
+        loop.create_task(send_message(socket_client))
+        
 
 async def send_message(socket_client):
+    print('Read')
     while True:
         request = socket_client.recv(4096)
 
@@ -22,16 +20,20 @@ async def send_message(socket_client):
             print('Client is closed')
             socket_client.close()
         else:
-            response = 'Hello, world\n'.encode()
-            socket_client.send(response)  
+            socket_client.send('Hello, world\n'.encode())
+        print('End read request')
 
 async def main():
-    task1 = asyncio.create_task(accept_connection(socket_server))
-    tasks = []
-    for i in clients:
-        tasks.append(asyncio.create_task(send_message()))
+    print('Start')
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_server.bind(('localhost',5000))
+    socket_server.listen()
+    print('Server is created')
 
-    await asyncio.gather(task1, *tasks)
+    loop = asyncio.get_running_loop()
+    loop.create_task(accept_connection(socket_server))
+    print('Server is added to loop')
 
 if __name__ == '__main__':
     asyncio.run(main())
