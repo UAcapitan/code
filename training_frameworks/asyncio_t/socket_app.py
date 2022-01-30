6e8f380ae28adf35cd106cbad6a6b397e4fd7186
EdMix <ua.capitan@gmail.com>
2022-01-31 00:29:45 +0200

    AB - sketch of server

--- training_frameworks/asyncio_t/socket_app.py
+++ training_frameworks/asyncio_t/socket_app.py
@@ -1,40 +1,20 @@
 import socket
-import asyncio
 
-async def accept_connection(socket_server):
-    print('Server is listening')
-    loop = asyncio.get_running_loop()
-    global clients
-    while True:
-        socket_client, addr = socket_server.accept()
-        print('Connection from', addr)
-        loop.create_task(send_message(socket_client))
-        print('Task was created')
-        
+class Socket:
+    def __init__(self):
+        self.socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        )
 
-async def send_message(socket_client):
-    print('Read')
-    while True:
-        request = socket_client.recv(4096)
+    def send_data(self, data):
+        raise NotImplementedError()
 
-        if not request:
-            print('Client is closed')
-            socket_client.close()
-        else:
-            socket_client.send('Hello, world\n'.encode())
-        print('End read request')
+    def listen_socket(self, listened_socket=None):
+        raise NotImplementedError()
 
-async def main():
-    print('Start')
-    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_server.bind(('localhost',5000))
-    socket_server.listen()
-    print('Server is created')
-
-    loop = asyncio.get_running_loop()
-    loop.create_task(accept_connection(socket_server))
-    print('Server is added to loop')
+    def set_up(self):
+        raise NotImplementedError()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    pass
