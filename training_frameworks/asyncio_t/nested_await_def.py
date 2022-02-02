354742955135884fed2ac6c5eeb3925fb4b1534e
EdMix <ua.capitan@gmail.com>
2022-02-02 13:33:33 +0200

    AB - use create_task

--- training_frameworks/asyncio_t/nested_await_def.py
+++ training_frameworks/asyncio_t/nested_await_def.py
@@ -1,10 +1,14 @@
 import asyncio
 
 async def nested():
+    print(1)
     return 1
 
 async def main():
-    print(await nested())
+    while True:
+        task = asyncio.create_task(nested())
+
+        await task
 
 if __name__ == '__main__':
     asyncio.run(main())
