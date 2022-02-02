2bdd10579a382b502a77fdf2517fe20d9e8c0379
EdMix <ua.capitan@gmail.com>
2022-02-02 13:14:54 +0200

    AB - working socket server

--- training_frameworks/asyncio_t/socket_app.py
+++ training_frameworks/asyncio_t/socket_app.py
@@ -3,40 +3,37 @@ import asyncio
 
 async def accept_connection(socket_server):
     print('Server is listening')
+    loop = asyncio.get_event_loop()
     while True:
-        loop = asyncio.get_event_loop()
         socket_client, addr = await loop.sock_accept(socket_server)
         print('Connection from', addr)
-        asyncio.create_task(send_message(socket_client))
+        loop.create_task(send_message(socket_client))
         print('Added')
-        await asyncio.sleep(1/1000)
 
 
 async def send_message(socket_client):
-    print('Read')
     loop = asyncio.get_event_loop()
-    request = await loop.sock_recv(socket_client, 4096)
-
-    if not request:
-        print('Client is closed')
-        socket_client.close()
-    else: 
-        await loop.sock_sendall(socket_client, 'Hello, world\n'.encode())
-    print('End read request')
-    socket_client.close()
-    await asyncio.sleep(1/1000)
+    while True:
+        print('Read')
+        print('Before sock recv')
+        request = await loop.sock_recv(socket_client, 4096)
+        print('After sock recv')
+        if not request:
+            socket_client.close()
+        else:
+            await loop.sock_sendall(socket_client, 'Hello, world\n'.encode())
+            print('End read request')
 
 async def main():
     print('Start')
     socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     socket_server.bind(('localhost',5000))
+    socket_server.setblocking(False)
     socket_server.listen()
     print('Server is created')
 
-    task = asyncio.create_task(accept_connection(socket_server))
-    asyncio.gather(task)
-    # await task
+    await accept_connection(socket_server)
 
 if __name__ == '__main__':
     asyncio.run(main())
