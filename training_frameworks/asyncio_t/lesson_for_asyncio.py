98d416432d213e638f73db7b9171fc050d8f95ad
EdMix <ua.capitan@gmail.com>
2022-02-02 08:52:17 +0200

    AB - lesson for asyncio (full code)

--- training_frameworks/asyncio_t/lesson_for_asyncio.py
+++ training_frameworks/asyncio_t/lesson_for_asyncio.py
@@ -11,3 +11,28 @@ async def print_every_1_sec(counter):
     while True:
         await asyncio.sleep(1)
         print(f'Length of counter after 1 second: {len(counter)}')
+
+async def print_every_5_sec(counter):
+    while True:
+        await asyncio.sleep(5)
+        print(f'Length of counter after 5 second: {len(counter)}')
+
+async def print_every_10_sec(counter):
+    while True:
+        await asyncio.sleep(10)
+        print(f'Length of counter after 10 second: {len(counter)}')
+
+async def main():
+    counter = []
+
+    tasks = [
+        count(counter),
+        print_every_1_sec(counter),
+        print_every_5_sec(counter),
+        print_every_10_sec(counter),
+    ]
+
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- training_frameworks/asyncio_t/socket_app.py
+++ training_frameworks/asyncio_t/socket_app.py
@@ -1,31 +1,31 @@
 import socket
 import asyncio
 
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_server.bind(('localhost',5000))
-socket_server.listen()
-
 async def accept_connection(socket_server):
     print('Server is listening')
-    loop = asyncio.get_event_loop()
     while True:
-        socket_client, addr = socket_server.accept()
+        loop = asyncio.get_event_loop()
+        socket_client, addr = await loop.sock_accept(socket_server)
         print('Connection from', addr)
         loop.create_task(send_message(socket_client))
+        print('Added')
+        await asyncio.sleep(1/1000)
 
 
 async def send_message(socket_client):
-    print('Read')
     while True:
-        request = socket_client.recv(4096)
+        print('Read')
+        loop = asyncio.get_event_loop()
+        request = await loop.sock_recv(socket_client, 4096)
 
         if not request:
             print('Client is closed')
             socket_client.close()
         else: 
-            socket_client.send('Hello, world\n'.encode())
+            await loop.sock_sendall(socket_client, 'Hello, world\n'.encode())
         print('End read request')
+        socket_client.close()
+        await asyncio.sleep(1)
 
 async def main():
     print('Start')
@@ -35,9 +35,9 @@ async def main():
     socket_server.listen()
     print('Server is created')
 
-    loop = asyncio.get_running_loop()
-    loop.create_task(accept_connection(socket_server))
-    print('Server is added to loop')
+    task = asyncio.create_task(accept_connection(socket_server))
+    # asyncio.gather(task)
+    await task
 
 if __name__ == '__main__':
     asyncio.run(main())
