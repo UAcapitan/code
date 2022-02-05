384d69fabb93c125a3631ec424dd61eaa0eeb0b9
EdMix <ua.capitan@gmail.com>
2022-02-06 00:05:19 +0200

    AB - client without with session get

---
+++ training_frameworks/aiohttp_t/client_2.py
@@ -0,0 +1,11 @@
+import asyncio
+import aiohttp
+
+async def main():
+    async with aiohttp.ClientSession('http://python.org') as session:
+        request = await session.get('/doc')
+        print(f'Status: {request.status}')
+        await session.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
