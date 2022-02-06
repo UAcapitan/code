06384826bc588502b9c34430d0397878ed714113
EdMix <ua.capitan@gmail.com>
2022-02-06 22:50:35 +0200

    FT - route and params

--- training_frameworks/fastapi_t/app.py
+++ training_frameworks/fastapi_t/app.py
@@ -3,7 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get('/')
 def home():
     return {'key':'test'}
+
+@app.get('/{pk}')
+def get_item(pk: int):
+    return {'key': pk}
+
+@app.get('/text/{q}')
+def get_text(q: str, t: str = None):
+    return {'key': q, 'text':t}
+
+@app.get('/test/{pk}/{t}')
+def get_test(pk: int, t: str):
+    return {'key': pk, 'text':t}
