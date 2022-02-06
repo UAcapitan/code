84f18b22dd0e248e933e0b80e4febfac8192227a
EdMix <ua.capitan@gmail.com>
2022-02-06 15:19:03 +0200

    AB - rename some files

---
+++ training_frameworks/aiohttp_t/client_iter_chunked.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.github.com/events') as request:
+            print(await request.content.read(200))
+            
+            with open('file.txt', 'wb') as file:
+                async for chunk in request.content.iter_chunked(10):
+                    file.write(chunk)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
