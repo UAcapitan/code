030c7f50a8e9a3966ecdc7f57f0408c2b6cb42d0
EdMix <ua.capitan@gmail.com>
2022-02-07 02:58:01 +0200

    FT - validator from pydantic

--- training_frameworks/fastapi_t/schemas.py
+++ training_frameworks/fastapi_t/schemas.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import date
 
 class Genre(BaseModel):
@@ -13,3 +13,9 @@ class Book(BaseModel):
     summary: str
     genres: List[Genre]
     pages: int
+
+    @validator('pages')
+    def check_pages(cls, v):
+        if v < 50:
+            raise ValueError('Pages of book must be more than 50')
+        return 50
