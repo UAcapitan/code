5ccfabc0da262882f5e2d32d407b4ecb72f3424e
EdMix <ua.capitan@gmail.com>
2022-02-07 03:04:58 +0200

    FT - Field from pydantic

--- training_frameworks/fastapi_t/schemas.py
+++ training_frameworks/fastapi_t/schemas.py
@@ -1,18 +1,24 @@
 from typing import List
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Field
 from datetime import date
 
 class Genre(BaseModel):
     name: str
 
 class Book(BaseModel):
-    title: str
+    title: str = Field(..., max_length=25)
     writer: str
     duration: str
     date: date
     summary: str
     genres: List[Genre]
     pages: int
+    count: int = Field(
+        ...,
+        gt=15,
+        lt=90,
+        description='Author age must be more than 15 and less than 90'
+    )
 
     @validator('pages')
     def check_pages(cls, v):
