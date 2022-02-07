af8122e805b08f42bb619a53489617b379a08660
EdMix <ua.capitan@gmail.com>
2022-02-07 22:58:09 +0200

    AB - aiohttp web server

---
+++ training_frameworks/aiohttp_t/server/server.py
@@ -0,0 +1,10 @@
+import asyncio
+from aiohttp import web
+
+async def main(request):
+    return web.Response(text='Hello, world')
+
+app = web.Application()
+app.add_routes([web.get('/main', main)])
+
+web.run_app(app)
