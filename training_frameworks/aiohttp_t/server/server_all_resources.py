28030178ea59b420d493d89930e37ce0dcdc0ef8
EdMix <ua.capitan@gmail.com>
2022-02-09 02:30:00 +0200

    AB - aiohttp server all lesources in cycle for

---
+++ training_frameworks/aiohttp_t/server/server_all_resources.py
@@ -0,0 +1,32 @@
+import asyncio
+from aiohttp import web
+
+
+routes = web.RouteTableDef()
+
+@routes.get('/', name='main')
+async def main(request):
+    return web.Response(text=f'Hello, world')
+
+@routes.get('/resources')
+async def resources(request):
+    url = request.app.router['main'].url_for().with_query({'a': 'b', 'c': 'd'})
+    return web.Response(text=str(url))
+
+@routes.get('/user-data')
+async def user_data(request):
+    url = request.app.router['user_info'].url_for(user='TestUserNew').with_query(
+        {'a':'b', 'c': 'd'}
+    )
+    return web.Response(text=str(url))
+
+@routes.get('/all')
+async def all(request):
+    return web.Response(text='\n'.join([str(i) for i in app.router.resources()]))
+
+
+app = web.Application()
+app.add_routes(routes)
+app.router.add_resource(r'/user/{user}/info', name='user_info')
+
+web.run_app(app, port=8080)
