93cae9af0dad68be7756e08a53a55c18a164772a
EdMix <ua.capitan@gmail.com>
2022-02-14 22:43:54 +0200

    GB - api pagination article

--- interesting_projects/Game_blog/gameblog/api/views.py
+++ interesting_projects/Game_blog/gameblog/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 import api.serializers as serializers
+from rest_framework.permissions import IsAdminUser
 import appmain.models as models
+import api.service as service
 
 class ArticleListView(generics.ListAPIView):
     serializer_class = serializers.ArticleSerializer
-    queryset = models.Article.objects.all()
+    queryset = models.Article.objects.all().order_by('-id')
+    pagination_class = service.PaginationArticle
