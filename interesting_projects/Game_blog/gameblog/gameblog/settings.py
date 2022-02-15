b2038e358a7cd3ee337b238eae07a0d583dd060f
EdMix <ua.capitan@gmail.com>
2022-02-16 01:16:37 +0200

    GB - api added swagger

--- interesting_projects/Game_blog/gameblog/gameblog/settings.py
+++ interesting_projects/Game_blog/gameblog/gameblog/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'drf_yasg',
-    
 ]
 
 MIDDLEWARE = [
--- interesting_projects/Game_blog/gameblog/gameblog/urls.py
+++ interesting_projects/Game_blog/gameblog/gameblog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin_panel'),
@@ -25,6 +26,8 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls.authtoken')),
     path('', include('appmain.urls')),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
