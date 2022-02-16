4b2f7092b96c84e427fb08f5e09e560ba668e02f
EdMix <ua.capitan@gmail.com>
2022-02-17 00:11:55 +0200

    GB - api show article by id

--- interesting_projects/Game_blog/gameblog/api/urls.py
+++ interesting_projects/Game_blog/gameblog/api/urls.py
@@ -4,4 +4,5 @@ import api.views as views
 urlpatterns = [
     path('articles/', views.ArticleListView.as_view(), name='articles'),
     path('rate/', views.RateView.as_view(), name='rate'),
+    path('article/<int:id>/', views.ArticleView.as_view(), name='article')
 ]
--- interesting_projects/Game_blog/gameblog/api/views.py
+++ interesting_projects/Game_blog/gameblog/api/views.py
@@ -3,6 +3,8 @@ import api.serializers as serializers
 from rest_framework.permissions import IsAdminUser
 import appmain.models as models
 import api.service as service
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class ArticleListView(generics.ListAPIView):
     serializer_class = serializers.ArticleSerializer
@@ -12,3 +14,9 @@ class ArticleListView(generics.ListAPIView):
 class RateView(generics.ListAPIView):
     serializer_class = serializers.RateSerializer
     queryset = models.Article.objects.all().order_by('-likes')[:10]
+
+class ArticleView(APIView):
+    def get(self, request, id):
+        movies = models.Article.objects.filter(id=id)
+        serializer = serializers.ArticleSerializer(movies, many=True)
+        return Response(serializer.data)
