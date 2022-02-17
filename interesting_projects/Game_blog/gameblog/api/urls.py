3041bb90f6ebc1870b59378cde8e89fe0016c303
EdMix <ua.capitan@gmail.com>
2022-02-17 19:58:18 +0200

    GB - show user profile by token

--- interesting_projects/Game_blog/gameblog/api/urls.py
+++ interesting_projects/Game_blog/gameblog/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('rate/', views.RateView.as_view(), name='rate'),
     path('article/<int:id>/', views.ArticleView.as_view(), name='article'),
     path('articles/names/', views.ArticleNameListView.as_view(), name='articles_names'),
+    path('profile/', views.UserDataView.as_view(), name='profile')
 ]
--- interesting_projects/Game_blog/gameblog/api/views.py
+++ interesting_projects/Game_blog/gameblog/api/views.py
@@ -5,6 +5,9 @@ import appmain.models as models
 import api.service as service
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
 class ArticleListView(generics.ListAPIView):
     serializer_class = serializers.ArticleSerializer
@@ -25,3 +28,22 @@ class ArticleNameListView(generics.ListAPIView):
     serializer_class = serializers.NameOfArticlesSerializer
     queryset = models.Article.objects.all().order_by('-id')
     pagination_class = service.PagonationArticlePageSize10
+
+class UserDataView(APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        json_dict = {}
+        user_token = request.data.get('token')
+
+        try:
+            token = Token.objects.get(key=user_token)
+            user = User.objects.get(username=token.user)
+            json_dict["id"] = user.id
+            json_dict["username"] = user.username
+            json_dict["email"] = user.email
+        except Exception as e:
+            json_dict["error"] = str(e)
+
+        return Response(json_dict)
+
+        
