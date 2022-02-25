436bf334d737e8637bd18b86f76feed5ab7d616e
EdMix <ua.capitan@gmail.com>
2022-02-25 22:20:28 +0200

    MyPy - List, Iterable, Union

--- training_modules/mypy_t/app.py
+++ training_modules/mypy_t/app.py
@@ -1,8 +1,11 @@
+from typing import *
+from collections.abc import *
+
 def greeting(name: str) -> str:
     return f'Hello, {name}'
 
 # print(greeting(3))
-print(greeting('Ivan'))
+# print(greeting('Ivan'))
 # print(greeting(b'Max'))
 
 def p() -> None:
@@ -39,5 +42,21 @@ def print_names(names: list[str]) -> None:
     for i in names:
         print(f'Hi, {i}')
 
-print(print_names(['Max', 'Ivan', 'Ira']))
-print(print_names([10,20,30]))
+# print(print_names(['Max', 'Ivan', 'Ira']))
+# print(print_names([10,20,30]))
+
+def print_list_of_names(names: List[str]) -> List[str]:
+    return names
+
+# print(['Max', 'John', 'Denis'])
+
+def iter_only(names: Iterable[str]) -> list:
+    return list(names)
+
+# print(iter_only(iter(['Max', 'Ivan', 'Denis'])))
+
+def str_and_none_only(name: Union[str, None]) -> Union[str, None]:
+    return name
+
+# print(str_and_none_only('Max'))
+# print(str_and_none_only(None))
