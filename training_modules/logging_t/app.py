67d61c3f53a1dd8db5aced44c88e5b8c6f395900
EdMix <ua.capitan@gmail.com>
2022-03-02 00:05:13 +0200

    Logging - debug for logging

--- training_modules/logging_t/app.py
+++ training_modules/logging_t/app.py
@@ -1,4 +1,12 @@
 import logging
 
-def main():
-    return 0
+logger = logging.getLogger(__name__)
+print(logger)
+
+def main(n):
+    logger.debug('Working')
+    n =+ 1
+    return n
+
+if __name__ == '__main__':
+    main(0)
