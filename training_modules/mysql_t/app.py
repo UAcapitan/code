8950c4f73811d229cee93c84a993a8a2da7e08ec
EdMix <ua.capitan@gmail.com>
2022-03-04 18:17:45 +0200

    Mysql - execute to db

--- training_modules/mysql_t/app.py
+++ training_modules/mysql_t/app.py
@@ -1,14 +1,16 @@
-from re import L
 from mysql.connector import connect, Error
 from getpass import getpass
 
 try:
     with connect(
-        host='localhost',
+        host='127.0.0.1',
         user=input('Input login: '),
         password=getpass('Input password: ')
     ) as connection:
         conn = connection
+        create_db_query = "CREATE DATABASE data_of_something"
+        with conn.cursor() as cursor:
+            cursor.execute(create_db_query)
 except Error as e:
     print(e)
 
