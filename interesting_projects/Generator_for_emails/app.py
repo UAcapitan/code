db6f158d2c4a0a56991f84efc393f80b995e0a7a
EdMix <ua.capitan@gmail.com>
2022-03-30 13:26:10 +0300

    GFE - add database model

--- interesting_projects/Generator_for_emails/app.py
+++ interesting_projects/Generator_for_emails/app.py
@@ -11,8 +11,21 @@ data_for_generation = {
         ['ua', 'ge', 'us', 'fr', 'com', 'net', 'org']
 }
 
+class DB:
+    def __init__(self) -> None:
+        self.db = peewee.SqliteDatabase('emails.db')
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = DB().db
+
+class EmailModule(BaseModel):
+    id = peewee.AutoField()
+    username = peewee.CharField(unique=True)
+    user = peewee.CharField()
+
 class App:
-    def __init__(self):
+    def __init__(self) -> None:
         self.root = tk.Tk()
 
 if __name__ == '__main__':
