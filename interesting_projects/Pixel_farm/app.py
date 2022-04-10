d9ea54ae7737f40bfa0cb2faa26b5ecc59d1145c
EdMix <ua.capitan@gmail.com>
2022-04-10 21:49:21 +0300

    PF - some new scr and basic screen

--- interesting_projects/Pixel_farm/app.py
+++ interesting_projects/Pixel_farm/app.py
@@ -1,21 +1,53 @@
 import pygame
 import sys
+import json
+import tkinter as tk
 
 pygame.init()
 
-SIZE = (100, 100)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 
 class PixelFarm:
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+            Constructor for setting up game
+        '''
+        
+        # Set up title
         pygame.display.set_caption('Pixel Farm')
 
-        self.screen = pygame.display.set_mode(SIZE)
+        # Load settings
+        with open('settings.json', 'r') as file:
+            self.settings = json.load(file)
 
-    def run(self):
+        # Set up of screen
+        if self.settings["fullscreen"]:
+            self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        else:
+            root = tk.Tk()
+
+            self.screen = pygame.display.set_mode((root.winfo_screenwidth()-100, root.winfo_screenheight()-100))
+
+    def run(self) -> None:
+        '''
+            Main function of game
+        '''
+
         while True:
+            
+            # Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == pygame.K_q:
+                    sys.exit()
+
+            # Fill screen
+            self.screen.fill(GREEN)
+
+            # Updating of screen
+            pygame.display.update()
 
 
 if __name__ == '__main__':
