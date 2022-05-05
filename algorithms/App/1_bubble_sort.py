cee19965ec254ea7ac0bbde8ba311a134ce6b1a1
EdMix <ua.capitan@gmail.com>
2022-05-06 00:43:20 +0300

    Algorithms - bubble sort

--- algorithms/App/1_bubble_sort.py
+++ algorithms/App/1_bubble_sort.py
@@ -1,9 +1,13 @@
 nums = [5,4,9,1,3,2,6,8,7]
 
 def bubble_sort(n):
-	p, l = len(n)-2, len(n)-1
-	for i in range(len(n)):
-		n[p], n[l] = n[l], n[p]
+	for i in range(len(n)-1):
+		for j in range(0, len(n)-1-i):
+			if n[j] > n[j+1]:
+				n[j], n[j+1] = n[j+1], n[j]
+	return n
 
 if __name__ == "__main__":
+	print(nums)
+	print("Results after sorting:\n")
 	print(bubble_sort(nums))
