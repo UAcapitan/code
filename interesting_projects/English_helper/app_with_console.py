ea7779078dc9babfffe79773dc7c9ef675f34169
EdMix <ua.capitan@gmail.com>
2022-05-12 01:38:34 +0300

    EH - update console app

--- interesting_projects/English_helper/app_with_console.py
+++ interesting_projects/English_helper/app_with_console.py
@@ -8,13 +8,50 @@ dir = __file__.replace('app_with_console.py', 'english.db')
 con = sqlite3.connect(dir)
 cur = con.cursor()
 
-while True:
-    cur.execute("SELECT * FROM words;")
-    words = cur.fetchall()
-    word_eng = random.choice(words)
-    word = input(Fore.WHITE + f"{word_eng[0]} - ")
-    if word == word_eng[1]:
-        print(Fore.GREEN + f"{word_eng[0]} - {word}")
-    else:
-        print(Fore.RED + f"{word_eng[0]} - {word_eng[1]}")
-    
+def gap():
+    for i in range(3):
+        print("")
+
+stage = int(input(
+"1. Translate from English\n\
+2. Translate to English\n\
+3. Limit 30 words\n\n"
+))
+
+gap()
+
+if stage == 1:
+    while True:
+        cur.execute("SELECT * FROM words;")
+        words = cur.fetchall()
+        word_eng = random.choice(words)
+        word = input(Fore.WHITE + f"{word_eng[0]} - ")
+        if word == word_eng[1]:
+            print(Fore.GREEN + f"{word_eng[0]} - {word}")
+        else:
+            print(Fore.RED + f"{word_eng[0]} - {word_eng[1]}")
+        gap()
+
+if stage == 2:
+    while True:
+        cur.execute("SELECT * FROM words;")
+        words = cur.fetchall()
+        word_eng = random.choice(words)
+        word = input(Fore.WHITE + f"{word_eng[1]} - ")
+        if word == word_eng[0]:
+            print(Fore.GREEN + f"{word_eng[1]} - {word}")
+        else:
+            print(Fore.RED + f"{word_eng[1]} - {word_eng[0]}")
+        gap()
+
+if stage == 3:
+    while True:
+        cur.execute("SELECT * FROM words;")
+        words = cur.fetchall()[:30]
+        word_eng = random.choice(words)
+        word = input(Fore.WHITE + f"{word_eng[0]} - ")
+        if word == word_eng[1]:
+            print(Fore.GREEN + f"{word_eng[0]} - {word}")
+        else:
+            print(Fore.RED + f"{word_eng[0]} - {word_eng[1]}")
+        gap()
