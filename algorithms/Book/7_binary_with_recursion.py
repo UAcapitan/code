ad805917d04f330120b0d8b47d3f1a54c0e7f44a
EdMix <ua.capitan@gmail.com>
2022-05-12 20:44:17 +0300

    Algorithms - binary with recursion (updated)

--- algorithms/Book/7_binary_with_recursion.py
+++ algorithms/Book/7_binary_with_recursion.py
@@ -1,15 +1,17 @@
 nums = [1,2,3,4,5,6,7,8,9]
 
-def binary_with_recursion(n, i):
+def binary_with_recursion(n, i, low=0, high=None):
+	if high is None:
+		high = len(n) - 1
 	if len(n) == 0:
 		return None
-	mid = len(n) // 2
+	mid = (low + high) // 2
 	if i == n[mid]:
 		return mid
 	else:
 		if i > n[mid]:
-			return binary_with_recursion(n[mid+1:], i)
+			return binary_with_recursion(n, i, mid+1, high)
 		else:
-			return binary_with_recursion(n[:mid], i)
+			return binary_with_recursion(n, i, low, mid-1)
 
-print(binary_with_recursion(nums, 3))
+print(binary_with_recursion(nums, 7))
