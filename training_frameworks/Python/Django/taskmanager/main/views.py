240ae3fad136213d205cf9a888b7df40d2bbe5d8
EdMix <ua.capitan@gmail.com>
2022-05-15 11:31:08 +0300

    TF Django - basic programming

--- training_frameworks/Python/Django/taskmanager/main/views.py
+++ training_frameworks/Python/Django/taskmanager/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from . import forms
 
 
 def index(request):
@@ -11,3 +12,16 @@ def index(request):
 
 def page(request):
     return render(request, 'main/page.html')
+
+def create(request):
+    form = forms.TaskForm()
+    context = {
+        'form': form,
+    }
+    if request.method == "POST":
+        form = forms.TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('main')
+    else:
+        return render(request, 'main/create.html', context)
