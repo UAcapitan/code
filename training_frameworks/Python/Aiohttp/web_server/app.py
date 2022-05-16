6ede54717488b3b1236ce8ca314eb670fd334e78
EdMix <ua.capitan@gmail.com>
2022-05-16 13:24:09 +0300

    TF Aiohttp - try to launch

--- training_frameworks/Python/Aiohttp/web_server/app.py
+++ training_frameworks/Python/Aiohttp/web_server/app.py
@@ -1,11 +1,28 @@
 import aiohttp
 from aiohttp import web
+import fcntl
+import ffilib
 
-async def create_app():
+
+async def index(request):
+    return web.Response(text='Hello, world!')
+
+async def page(request):
+    return web.Response(text='Just a page')
+
+async def create(request):
+    return web.Response(text='This is form')
+
+
+def create_app():
     app = web.Application()
+    app.router.add_get("/", index)
+    app.router.add_get("/page", page)
+    app.router.add_get("/create", create)
     return app
 
 app = create_app()
 
+
 if __name__ == '__main__':
-    aiohttp.web.run_app(app,)
+    web.run_app(app)
