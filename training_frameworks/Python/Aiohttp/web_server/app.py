07fe0454f3cefde89697eed6d861d00d6e9135d6
EdMix <ua.capitan@gmail.com>
2022-05-17 23:57:58 +0300

    TF Aiohttp - model for database

--- training_frameworks/Python/Aiohttp/web_server/app.py
+++ training_frameworks/Python/Aiohttp/web_server/app.py
@@ -10,8 +10,14 @@ db_engine = sqlalchemy.create_engine('sqlite:///tasks.db')
 Base = declarative_base()
 
 class Task(Base):
+    __tablename__ = "task"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    title = sqlalchemy.Column(sqlalchemy.String(50))
+
+
     def __repr__(self):
-        return
+        return self.title
 
 @template('index.html')
 async def index(request):
