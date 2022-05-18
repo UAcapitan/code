59da0fbd7de3889973966d4662bd870bbbc1bfdc
EdMix <ua.capitan@gmail.com>
2022-05-19 01:42:43 +0300

    TF Aiohttp - query with data

--- training_frameworks/Python/Aiohttp/client/app.py
+++ training_frameworks/Python/Aiohttp/client/app.py
@@ -1,10 +1,16 @@
 import aiohttp
 import asyncio
 
+
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://google.com') as resp:
             print(resp.status)
+
+        async with session.post('http://httpbin.org/post', data=b'data') as resp:
+            data = await resp.text()
+            print(data)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
