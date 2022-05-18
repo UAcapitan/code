76438433d20a5d00a21b4a4b59c0678ddda11c29
EdMix <ua.capitan@gmail.com>
2022-05-19 00:08:41 +0300

    TF Aiohttp - client get query

--- training_frameworks/Python/Aiohttp/client/app.py
+++ training_frameworks/Python/Aiohttp/client/app.py
@@ -1,4 +1,10 @@
 import aiohttp
+import asyncio
 
 async def main():
-    pass
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://google.com') as resp:
+            print(resp.status)
+
+if __name__ == "__main__":
+    asyncio.run(main())
