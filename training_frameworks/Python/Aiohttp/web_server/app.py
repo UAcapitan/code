21ff8087d9dbc40873434aa3d605f451752d2f20
EdMix <ua.capitan@gmail.com>
2022-05-18 12:17:43 +0300

    TF Aiohttp - set model and migrate

--- training_frameworks/Python/Aiohttp/web_server/app.py
+++ training_frameworks/Python/Aiohttp/web_server/app.py
@@ -1,10 +1,11 @@
+from asyncio import tasks
 from aiohttp import web
 import aiohttp_jinja2
 import jinja2
 from aiohttp_jinja2 import template
-import sqlite3
 import sqlalchemy
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, Session, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 db_engine = sqlalchemy.create_engine('sqlite:///tasks.db')
 Base = declarative_base()
@@ -13,7 +14,9 @@ class Task(Base):
     __tablename__ = "task"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    title = sqlalchemy.Column(sqlalchemy.String(50))
+    title = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
+    text = sqlalchemy.Column(sqlalchemy.Text, default="None")
+    mark = sqlalchemy.Column(sqlalchemy.Integer, default=5)
 
 
     def __repr__(self):
@@ -21,7 +24,10 @@ class Task(Base):
 
 @template('index.html')
 async def index(request):
-    return {}
+    with Session(db_engine) as session:
+        query = session.query(Task)
+        all_tasks = query.all()
+    return {"tasks": all_tasks}
 
 @template('page.html')
 async def page(request):
@@ -38,8 +44,11 @@ async def create_post(request):
     text = data['text']
     mark = data['mark']
     print(f"{title} {text} {mark}")
+    with Session(db_engine) as session:
+        task = Task(title=title, text=text, mark=mark)
+        session.add(task)
+        session.commit()
     raise web.HTTPFound('/')
-    # return {text: 'text'}
 
 @template('success.html')
 async def success(request):
@@ -64,5 +73,13 @@ def all_routes(app):
     app.router.add_post("/create", create_post)
     app.router.add_get("/success", success)
 
+def start_database():
+    if not database_exists(db_engine.url):
+        create_database(db_engine.url)
+    session = sessionmaker()
+    session.configure(bind=db_engine)
+    Base.metadata.create_all(db_engine)
+
 if __name__ == '__main__':
+    start_database()
     web.run_app(app_launch(), host='127.0.0.1', port=3000)
