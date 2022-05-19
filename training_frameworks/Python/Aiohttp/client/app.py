2539dd214ecbcb69d25cc2ac8f25afd337bf260e
EdMix <ua.capitan@gmail.com>
2022-05-19 13:48:53 +0300

    TF Aiohttp - uncomment comments

--- training_frameworks/Python/Aiohttp/client/app.py
+++ training_frameworks/Python/Aiohttp/client/app.py
@@ -13,24 +13,24 @@ async def main():
 
 
     async with aiohttp.ClientSession() as session:
-        # async with session.get('https://google.com') as resp:
-        #     print(resp.status)
+        async with session.get('https://google.com') as resp:
+            print(resp.status)
 
-        # async with session.post('http://httpbin.org/post', data=b'data') as resp:
-        #     data = await resp.text()
-        #     print(data)
+        async with session.post('http://httpbin.org/post', data=b'data') as resp:
+            data = await resp.text()
+            print(data)
 
         url = 'https://httpbin.org/get'
         url_post = 'https://httpbin.org/post'
 
-        # async with session.get(url, params=params) as resp:
-        #     print(resp.url) 
+        async with session.get(url, params=params) as resp:
+            print(resp.url) 
 
-        # async with session.post(url_post, json={"text": "text"}) as resp:
-        #     print(await resp.json())
+        async with session.post(url_post, json={"text": "text"}) as resp:
+            print(await resp.json())
 
-        # async with session.get(url) as resp:
-        #     print(await resp.content.read(10))
+        async with session.get(url) as resp:
+            print(await resp.content.read(10))
 
         async with session.post(url_post, data=data_for_file) as resp:
             print(await resp.json())
