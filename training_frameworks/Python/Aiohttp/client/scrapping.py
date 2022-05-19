067949d0e81d0901bda4521dc3706a200e225651
EdMix <ua.capitan@gmail.com>
2022-05-19 14:26:57 +0300

    TF Aiohttp - scrapping from page

---
+++ training_frameworks/Python/Aiohttp/client/scrapping.py
@@ -0,0 +1,38 @@
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+async def get_page(session, url):
+    async with session.get(url) as r:
+        return await r.text()
+
+async def get_all(session, urls):
+    tasks = []
+    for url in urls:
+        task = asyncio.create_task(get_page(session, url))
+        tasks.append(task)
+    results = await asyncio.gather(*tasks)
+    return results
+
+async def main(urls):
+    async with aiohttp.ClientSession() as session:
+        data = await get_all(session, urls)
+        return data
+
+def parse(results):
+    for html in results:
+        soup = BeautifulSoup(html)
+        print(soup.find('form', {'class': 'form-horizontal'}).text.strip())
+    return "Done"
+
+
+if __name__ == "__main__":
+    urls = [
+        'https://books.toscrape.com/catalogue/page-1.html',
+        'https://books.toscrape.com/catalogue/page-2.html',
+        'https://books.toscrape.com/catalogue/page-3.html',
+    ]
+
+    results = asyncio.run(main(urls))
+    print(len(results))
+    print(parse(results))
