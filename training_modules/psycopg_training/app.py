24fe2b452e03d0db8d5f157feaa5ec261561507c
EdMix <ua.capitan@gmail.com>
2022-05-20 12:12:32 +0300

    TM Psycopg - first query execute

--- training_modules/psycopg_training/app.py
+++ training_modules/psycopg_training/app.py
@@ -2,3 +2,6 @@ import psycopg2
 conn = psycopg2.connect(dbname='blog', user='postgres')
 cursor = conn.cursor()
 
+cursor.execute("CREATE DATABASE online;")
+records = cursor.fetchall()
+print(records)
