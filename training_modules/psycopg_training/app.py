fefb3c88ad051e60a17222aaca59ac2a4d9581b9
EdMix <ua.capitan@gmail.com>
2022-05-20 13:17:36 +0300

    TM Aiohttp - success with connection

--- training_modules/psycopg_training/app.py
+++ training_modules/psycopg_training/app.py
@@ -1,7 +1,15 @@
 import psycopg2
-conn = psycopg2.connect(dbname='blog', user='postgres')
+conn = psycopg2.connect(dbname='blog', user='app', password='thisispassword')
 cursor = conn.cursor()
 
-cursor.execute("CREATE DATABASE online;")
+cursor.execute("SELECT * from users;")
+records = cursor.fetchall()
+print(records)
+
+cursor.execute("SELECT * from post;")
+records = cursor.fetchall()
+print(records)
+
+cursor.execute("SELECT * from likes;")
 records = cursor.fetchall()
 print(records)
