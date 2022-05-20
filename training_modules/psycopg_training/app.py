6d53eff6cd1473847c511b917c74d45ef14ce571
EdMix <ua.capitan@gmail.com>
2022-05-20 21:17:40 +0300

    TF - new query for counting comments

--- training_modules/psycopg_training/app.py
+++ training_modules/psycopg_training/app.py
@@ -2,14 +2,20 @@ import psycopg2
 conn = psycopg2.connect(dbname='blog', user='app', password='thisispassword')
 cursor = conn.cursor()
 
-cursor.execute("SELECT * from users;")
-records = cursor.fetchall()
-print(records)
+def command(query):
+    cursor.execute(query)
+    return cursor.fetchall()
 
-cursor.execute("SELECT * from post;")
-records = cursor.fetchall()
-print(records)
+def print_results(list_):
+    if list_:
+        for i in list_:
+            print(i)
+    else:
+        print(None)
 
-cursor.execute("SELECT * from likes;")
-records = cursor.fetchall()
-print(records)
+def query_to_db(query):
+    print_results(command(query))
+
+if __name__ == "__main__":
+    # query_to_db("SELECT * FROM users;")
+    query_to_db("SELECT u.name, count(c) from users u left join comments c on u.id=c.author group by u.name;")
