69eb4a3c3b0c666beba9bcb712cad01bcdfea2a7
EdMix <ua.capitan@gmail.com>
2022-05-22 01:09:44 +0300

    TM Elasticsearch - connect to database

--- training_modules/elasticsearch_training/app.py
+++ training_modules/elasticsearch_training/app.py
@@ -1,4 +1,9 @@
 from elasticsearch import Elasticsearch
+import psycopg2
+
+def get_cursor():
+    conn = psycopg2.connect(dbname="blog", user="app", password="thisispassword")
+    return conn.cursor()
 
 client = Elasticsearch("http://localhost:3000")
 
