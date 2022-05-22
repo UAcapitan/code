894365d45d6dbc80bcad3804b3726f3059225d4d
EdMix <ua.capitan@gmail.com>
2022-05-23 00:26:23 +0300

    TM Elasticsearch - added in index

--- training_modules/elasticsearch_training/app.py
+++ training_modules/elasticsearch_training/app.py
@@ -1,10 +1,37 @@
 from elasticsearch import Elasticsearch
 import psycopg2
+import keys
+import random
 
-def get_cursor():
-    conn = psycopg2.connect(dbname="blog", user="app", password="thisispassword")
-    return conn.cursor()
 
-client = Elasticsearch("http://localhost:3000")
+conn = psycopg2.connect(dbname="blog", user="app", password="thisispassword")
+curs = conn.cursor()
 
-client.info()
+host = ''
+port = 0
+dbname = ''
+user = ''
+password = ''
+
+es = Elasticsearch(hosts=f"http://{keys.username}:{keys.password}@localhost:9200/")
+
+for i in range(8):
+
+    doc = {
+        'id':i,
+        'name':f'Name_{i}',
+        'password':'1234567',
+        'age':random.randint(16,70),
+        'rate':random.randint(0,10)
+    }
+
+    resp = es.index(index="post", id=i, document=doc)
+    print(resp['result'])
+
+es.indices.refresh(index="post")
+
+resp = es.search(index="post", query={"match_all": {}})
+
+print(resp["hits"]["hits"])
+
+# es.info()
