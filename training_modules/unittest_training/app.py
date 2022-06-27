d4a4d29bac344c2a128df865996a592cf587925c
EdMix <ua.capitan@gmail.com>
2022-06-28 01:04:10 +0300

    UT - first unit tests

--- training_modules/unittest_training/app.py
+++ training_modules/unittest_training/app.py
@@ -1,3 +1,15 @@
-def calculator(expression):
-    if len(expression) != 3:
+def calculator(e):
+    if len(e) != 3:
         raise ValueError('Incorrect data')
+    try:
+        a, sym, b = int(e[0]), e[1], int(e[2])
+    except ValueError:
+        raise ValueError('Incorrect type of data')
+    if sym == '+':
+        return a + b
+    if sym == '-':
+        return a - b
+    if sym == '*':
+        return a * b
+    if sym == '/':
+        return a / b
