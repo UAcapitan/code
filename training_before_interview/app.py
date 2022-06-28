33b3ad456a84906ffa23612e066e5e6a7ad0ad80
EdMix <ua.capitan@gmail.com>
2022-06-28 19:06:59 +0300

    TBI - class

--- training_before_interview/app.py
+++ training_before_interview/app.py
@@ -27,10 +27,43 @@ def coroutine():
 # cor.send(False)
 
 list_ = [i for i in [1,2,3]]
-print(list_)
+# print(list_)
 dict_ = {k:b for b, k in {'1':'a', '2':'b', '3':'c'}.items()}
-print(dict_)
+# print(dict_)
 set_ = {i for i in [1,2,3,4,5,5,5]}
-print(set_)
+# print(set_)
 generator_ = (i*2 for i in range(5))
-print(generator_)
+# print(generator_)
+
+class Test:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def sum_all(self):
+        return self.a + self.b + self.c
+
+    def __str__(self):
+        return str(self.sum_all())
+
+# test = Test(1,7,2)
+# print(test.sum_all())
+# print(test)
+
+class Test2(Test):
+    def sum_all(self):
+        return 0
+
+# test = Test2(1,7,2)
+# print(test)
+
+class Test:
+    def __init__(self, a):
+        self.a = a
+
+    def __getitem__(self, i):
+        return self.a[i]
+
+# test = Test({'a':'a', 'b':'b', 'c':'c'})
+# print(test['a'])
