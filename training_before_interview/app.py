34a2fe635bb42c028d6a3fa52f4c1af641cd37da
EdMix <ua.capitan@gmail.com>
2022-06-30 12:28:49 +0300

    TBI - itertools

--- training_before_interview/app.py
+++ training_before_interview/app.py
@@ -84,4 +84,23 @@ def test(f):
 def new_test():
     print('I am working')
 
-new_test()
+# new_test()
+
+# print(os.name)
+# print(os.mkdir('test'))
+# print(os.getcwd())
+
+import itertools
+
+list_ = [
+    itertools.accumulate([1,2,3,4,5]),
+    itertools.combinations('ABCDF', 3),
+    itertools.compress('ABCDF', '01'),
+    itertools.dropwhile(lambda x: x < 5, [1,2,3,4,5,6,7,8,9]),
+    itertools.product('ABC', 'xy'),
+    itertools.takewhile(lambda x: x >= 5, [7,6,5,4,3,2,1]),
+    itertools.zip_longest('ABCD', 'xy', fillvalue='-')
+]
+
+for i in list_:
+    print(list(i))
