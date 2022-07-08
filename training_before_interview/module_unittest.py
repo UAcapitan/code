de92763284d9777bdf1384ca35edecb2d9dd73d8
EdMix <ua.capitan@gmail.com>
2022-07-08 20:25:59 +0300

    TBI - unittest type raises

--- training_before_interview/module_unittest.py
+++ training_before_interview/module_unittest.py
@@ -25,8 +25,24 @@ class Tests(unittest.TestCase):
 
         self.assertTrue('Empty field error' in context.exception)
 
+    def test2(self):
+        with self.assertRaises(Exception) as context:
+            tested_function(42)
 
-        
+        self.assertTrue('Type of field error' in context.exception)
+
+        with self.assertRaises(Exception) as context:
+            tested_function(0.7)
+
+        self.assertTrue('Type of field error' in context.exception)
+
+        with self.assertRaises(Exception) as context:
+            tested_function({'1': 'test'})
+
+        self.assertTrue('Type of field error' in context.exception)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
