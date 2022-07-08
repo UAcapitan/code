b820b6ab6b5b2ba1152a565012703da3d0a7afb8
EdMix <ua.capitan@gmail.com>
2022-07-08 18:25:58 +0300

    TBI - unittest raises

--- training_before_interview/module_unittest.py
+++ training_before_interview/module_unittest.py
@@ -8,6 +8,25 @@ class Tests(unittest.TestCase):
         self.assertEqual(tested_function('Hello world'), 'World hello')
         self.assertEqual(tested_function('I love you'), 'You love I')
         self.assertEqual(tested_function('Word'), 'Word')
+    
+    def test2(self):
+        with self.assertRaises(Exception) as context:
+            tested_function()
+
+        self.assertTrue('Empty field error' in context.exception)
+
+        with self.assertRaises(Exception) as context:
+            tested_function([])
+
+        self.assertTrue('Empty field error' in context.exception)
+
+        with self.assertRaises(Exception) as context:
+            tested_function('')
+
+        self.assertTrue('Empty field error' in context.exception)
+
+
+        
 
 if __name__ == "__main__":
     unittest.main()
