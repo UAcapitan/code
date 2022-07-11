a522bc5d75780ac05bb409227769792882f14294
EdMix <ua.capitan@gmail.com>
2022-07-11 10:45:54 +0300

    TBI - aiohtpp recreate server

--- training_before_interview/module_aiohttp_server.py
+++ training_before_interview/module_aiohttp_server.py
@@ -1,8 +1,11 @@
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
+@routes.get('/')
 async def hi(request):
     return web.Response(text='Hi, world!')
 
 app = web.Application()
-app.add_routes([web.get('/', hi)])
+app.add_routes(routes)
 web.run_app(app)
