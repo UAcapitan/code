4dc1c2a33bbefc5a8a121d7f850dab248a0a6dff
EdMix <ua.capitan@gmail.com>
2022-07-12 19:38:00 +0300

    TBI - aiohttp client request

--- training_before_interview/module_aiohttp_client.py
+++ training_before_interview/module_aiohttp_client.py
@@ -2,7 +2,9 @@ import aiohttp
 import asyncio
 
 async def main():
-    pass
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://httpbin.org/get') as res:
+            print(res.status)
 
 if __name__ == "__main__":
-    asyncio.run(main)
+    asyncio.run(main())
