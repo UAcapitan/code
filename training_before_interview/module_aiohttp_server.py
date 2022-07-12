926791c8ba5bf0fc2017b10c262d02dbd8578572
EdMix <ua.capitan@gmail.com>
2022-07-12 19:32:33 +0300

    TBI - aiohttp server post

--- training_before_interview/module_aiohttp_server.py
+++ training_before_interview/module_aiohttp_server.py
@@ -6,6 +6,10 @@ routes = web.RouteTableDef()
 async def hi(request):
     return web.Response(text='Hi, world!')
 
+@routes.post('/')
+async def hi(request):
+    return web.Response(text='Hello, world!')
+
 app = web.Application()
 app.add_routes(routes)
 web.run_app(app)
