0a3ad6635694278e95d64bd1933fb13ea373a4be
EdMix <ua.capitan@gmail.com>
2022-07-25 06:31:38 +0300

    JTB - parse from dou

--- interesting_projects/Job_telegram_bot/app.py
+++ interesting_projects/Job_telegram_bot/app.py
@@ -1,3 +1,4 @@
+from unittest import result
 import telebot
 import requests
 from bs4 import BeautifulSoup
@@ -18,8 +19,11 @@ def start_bot(message):
     markup.add(button3)
     bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
 
-def get_page(url):
-    return requests.get(url).text
+def get_page(url, headers=''):
+    if headers == '':
+        return requests.get(url).text
+    else:
+        return requests.get(url, headers=headers).text
 
 def parse_djinni(url, n=0):
     soup = BeautifulSoup(get_page(url), "html.parser")
@@ -43,21 +47,34 @@ def parse_djinni(url, n=0):
 
             " ".join(i.find("div", class_="list-jobs__details")
             .find("div", class_="list-jobs__details__info").find("span", "location-text").text.strip().split()),
-
-            i.find("div", class_="list-jobs__details")
-            .find("div", class_="list-jobs__details__info")
-            .find_all("nobr")[0].text.strip(),
-
-            i.find("div", class_="list-jobs__details")
-            .find("div", class_="list-jobs__details__info")
-            .find_all("nobr")[1].text.replace("·", "").strip(),
-
-            i.find("div", class_="list-jobs__details")
-            .find("div", class_="list-jobs__details__info").find_all("nobr")[-1].text.replace("·", "").strip(),
         ])
 
         if vacancies[-1][-1] == vacancies[-1][-2]:
             vacancies[-1].pop()
+
+    return "\n\n".join(["\n".join(i) for i in vacancies])
+
+def parse_dou(url, n=0):
+    headers = {"User-Agent":"Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"}
+
+    soup = BeautifulSoup(get_page(url, headers), "html.parser")
+    results = soup.find_all("div", class_="vacancy")
+
+    if not results:
+        return 'No vacancies'
+
+    vacancies = []
+
+    if n > 0:
+        results = results[:n]
+
+    for i in results:
+        vacancies.append([
+            i.find("div", class_="title").find("a").text.replace("\xa0", " "),
+            i.find("div", class_="title").find("a")["href"],
+            i.find("div", class_="title").find("strong").find("a").text.encode("ascii", "ignore").decode(),
+            i.find("div", class_="title").find("span").text
+        ])
 
     return "\n\n".join(["\n".join(i) for i in vacancies])
 
@@ -91,5 +108,9 @@ def main(message):
         bot.send_message(message.chat.id, parse_djinni("https://djinni.co/jobs/?keywords=Junior+Python", 10))
     elif message.text == 'Djinni Trainee Python':
         bot.send_message(message.chat.id, parse_djinni("https://djinni.co/jobs/?keywords=Trainee+Python"))
-    
+    elif message.text == 'DOU Junior Python':
+        bot.send_message(message.chat.id, parse_dou("https://jobs.dou.ua/vacancies/?search=junior+python")),
+    elif message.text == 'DOU Trainee Python':
+        bot.send_message(message.chat.id, parse_dou("https://jobs.dou.ua/vacancies/?search=trainee+python"))
+
 bot.polling(none_stop=True)
