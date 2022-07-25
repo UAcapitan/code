72a41f06b5fa2c71912d2ca98100ba5ce84bdf21
EdMix <ua.capitan@gmail.com>
2022-07-25 06:58:04 +0300

    JTB - parse from jooble

--- interesting_projects/Job_telegram_bot/app.py
+++ interesting_projects/Job_telegram_bot/app.py
@@ -13,7 +13,7 @@ def start_bot(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton('Djinni')
     button2 = types.KeyboardButton('DOU')
-    button3 = types.KeyboardButton('')
+    button3 = types.KeyboardButton('Jooble')
     markup.add(button1)
     markup.add(button2)
     markup.add(button3)
@@ -78,6 +78,33 @@ def parse_dou(url, n=0):
 
     return "\n\n".join(["\n".join(i) for i in vacancies])
 
+def parse_jooble(url, n=0):
+    soup = BeautifulSoup(get_page(url), "html.parser")
+    results = soup.find_all("article", {"data-test-name": "_jobCard"})
+
+    if not results:
+        return 'No vacancies'
+
+    vacancies = []
+
+    if n > 0:
+        results = results[:n]
+
+    for i in results:
+        vacancies.append([
+            i.find("header").find("div").find("h2").find("a").text,
+
+            i.find("header").find("div").find("h2").find("a")["href"],
+
+            i.find("section").find("div", class_="_15xYk4")
+            .find("div").find("div").find("div").find("p").text,
+
+            i.find("section").find("div", class_="_15xYk4")
+            .find("div").find_all("div")[2].find("div").text
+        ])
+
+    return "\n\n".join(["\n".join(i) for i in vacancies])
+
 def djinni(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = types.KeyboardButton('Djinni Junior Python')
@@ -98,12 +125,24 @@ def dou(message):
     markup.add(button3)
     bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
 
+def jooble(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('Jooble Junior Python')
+    button2 = types.KeyboardButton('Jooble Trainee Python')
+    button3 = types.KeyboardButton('/back')
+    markup.add(button1)
+    markup.add(button2)
+    markup.add(button3)
+    bot.send_message(message.chat.id, 'Keyboard', reply_markup=markup)
+
 @bot.message_handler(content_types=['text'])
 def main(message):
     if message.text == 'Djinni':
         djinni(message)
     elif message.text == 'DOU':
         dou(message)
+    elif message.text == 'Jooble':
+        jooble(message)
     elif message.text == 'Djinni Junior Python':
         bot.send_message(message.chat.id, parse_djinni("https://djinni.co/jobs/?keywords=Junior+Python", 10))
     elif message.text == 'Djinni Trainee Python':
@@ -112,5 +151,9 @@ def main(message):
         bot.send_message(message.chat.id, parse_dou("https://jobs.dou.ua/vacancies/?search=junior+python")),
     elif message.text == 'DOU Trainee Python':
         bot.send_message(message.chat.id, parse_dou("https://jobs.dou.ua/vacancies/?search=trainee+python"))
+    elif message.text == 'Jooble Junior Python':
+        bot.send_message(message.chat.id, parse_jooble("https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=junior%20python", 10)),
+    elif message.text == 'Jooble Trainee Python':
+        bot.send_message(message.chat.id, parse_jooble("https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=trainee%20python", 10))
 
 bot.polling(none_stop=True)
