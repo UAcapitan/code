dbf67cc5ef70d3860b2a1306ecbca19a3af0c71b
EdMix <ua.capitan@gmail.com>
2022-07-30 01:24:53 +0300

    JTB - add parcing from grc

--- interesting_projects/Job_telegram_bot/app.py
+++ interesting_projects/Job_telegram_bot/app.py
@@ -1,8 +1,10 @@
+
 import telebot
 import requests
 from bs4 import BeautifulSoup
 from telebot import types
 import keys
+
 
 TOKEN = keys.TOKEN
 
@@ -11,8 +13,10 @@ bot = telebot.TeleBot(TOKEN)
 buttons = {
     'Djinni': ['Djinni Junior Python', 'Djinni Trainee Python'],
     'DOU': ['DOU Junior Python', 'DOU Trainee Python'],
-    'LinkedIn': ['LinkedIn Junior Python', 'LinkedIn Trainee Python']
-    # 'Jooble': ['Jooble Junior Python', 'Jooble Trainee Python']
+    'LinkedIn': ['LinkedIn Junior Python', 'LinkedIn Trainee Python'],
+    'Jooble': ['Jooble Junior Python', 'Jooble Trainee Python'],
+    'GRC': ['GRC Junior Python', 'GRC Trainee Python'],
+    'Work': ['Work Junior Python', 'Work Trainee Python']
 }
 
 urls = {
@@ -23,35 +27,38 @@ urls = {
     "LinkedIn Junior Python": "https://www.linkedin.com/jobs/search/?geoId=90010216&keywords=junior%20python&location=Kyiv%20Metropolitan%20Area",
     "LinkedIn Trainee Python": "https://www.linkedin.com/jobs/search/?geoId=90010216&keywords=trainee%20python&location=Kyiv%20Metropolitan%20Area",
     "Jooble Junior Python": "https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=junior%20python",
-    "Jooble Trainee Python": "https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=trainee%20python"
+    "Jooble Trainee Python": "https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=trainee%20python",
+    "GRC Junior Python": "https://grc.ua/search/vacancy?text=junior+python&from=suggest_post&fromSearchLine=true&area=115",
+    "GRC Trainee Python": "https://grc.ua/search/vacancy?text=trainee+python&from=suggest_post&fromSearchLine=true&area=115",
+    "Work Junior Python": "https://www.work.ua/jobs-kyiv-junior+python/",
+    "Work Trainee Python": "https://www.work.ua/jobs-kyiv-trainee+python/"
 }
+
 
 @bot.message_handler(commands=['start', 'back'])
 def start_bot(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton('Djinni')
-    button2 = types.KeyboardButton('DOU')
-    button3 = types.KeyboardButton('Jooble')
-    markup.add(button1)
-    markup.add(button2)
-    markup.add(button3)
+    
+    for i in buttons:
+        markup.add(types.KeyboardButton(i))
+
     bot.send_message(message.chat.id, 'Main keyboard', reply_markup=markup)
+
 
 def get_page(url, headers):
     return requests.get(url, headers=headers).text
 
+
 def get_buttons(message, site, buttons):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    list_ = []
+
     for i in buttons:
-        list_.append(types.KeyboardButton(i))
+        markup.add(types.KeyboardButton(i))
 
-    list_.append('/back')
-
-    for i in list_:
-        markup.add(i)
+    markup.add(types.KeyboardButton("/back"))
 
     bot.send_message(message.chat.id, f"{site} buttons", reply_markup=markup)
+
 
 def parse(message, site, url):
     headers = {"User-Agent":"Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"}
@@ -66,6 +73,10 @@ def parse(message, site, url):
             list_ = parse_linkedin(soup)
         case 'Jooble':
             list_ = parse_jooble(soup)
+        case 'GRC':
+            list_ = parce_grc(soup)
+        case 'Work':
+            list_ = parce_work(soup)
 
     if not list_:
         bot.send_message(message.chat.id, 'No vacancies')
@@ -73,6 +84,8 @@ def parse(message, site, url):
         text = "\n\n".join(["\n".join(i) for i in list_])
         bot.send_message(message.chat.id, text)
 
+
+# Parsers
 def parse_djinni(soup):
     vacancies = []
     results = soup.find_all("li", class_="list-jobs__item")
@@ -94,6 +107,7 @@ def parse_djinni(soup):
 
     return vacancies
 
+
 def parse_dou(soup):
     vacancies = []
     results = soup.find_all("div", class_="vacancy")
@@ -108,6 +122,7 @@ def parse_dou(soup):
 
     return vacancies
 
+
 def parse_linkedin(soup):
     vacancies = []
     results = soup.find_all("li", {"class": "ember-view jobs-search-results__list-item occludable-update p0 relative"})
@@ -119,6 +134,7 @@ def parse_linkedin(soup):
         ])
 
     return vacancies
+
 
 def parse_jooble(soup):
     vacancies = []
@@ -139,6 +155,39 @@ def parse_jooble(soup):
 
     return vacancies
 
+
+def parce_grc(soup):
+    vacancies = []
+    results = soup.find_all("div", {"class": "vacancy-serp-item-body"})
+
+    for i in results[:10]:
+        vacancies.append([
+            i.find_all("a", {"class": "bloko-link"})[0].text,
+            i.find_all("a", {"class": "bloko-link"})[0]["href"],
+            i.find_all("a", {"class": "bloko-link"})[1].text,
+            i.find("div", {"data-qa": "vacancy-serp__vacancy-address"}).text
+        ])
+
+    return vacancies
+
+
+def parce_work(soup):
+    vacancies = []
+    results = soup.find_all("li", {"class": "b-vacancy__item js-item_list"})
+    print(results)
+
+    for i in results[:10]:
+        vacancies.append([
+            i.find("a", {"class": "b-vacancy__top__title js-item_title"}).text,
+            i.find("a", {"class": "b-vacancy__top__title js-item_title"})["href"],
+            i.find("span", {"class": "b-vacancy__tech__item"}).text.strip(),
+            i.find("span", {"class": "b-vacancy__tech__item b-vacancy__tech__item-city"}).text.strip()
+        ])
+
+    return vacancies
+
+
+# Main bot's function
 @bot.message_handler(content_types=['text'])
 def main(message):
     if message.text in buttons:
@@ -146,5 +195,6 @@ def main(message):
 
     if message.text in urls:
         parse(message, message.text.split()[0], urls[message.text])
+
 
 bot.polling(none_stop=True)
