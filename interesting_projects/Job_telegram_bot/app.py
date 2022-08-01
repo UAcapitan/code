52f68f248514ac85c386195cd9cd6f93014981c2
EdMix <ua.capitan@gmail.com>
2022-08-01 03:58:17 +0300

    JTB - add from all sites

--- interesting_projects/Job_telegram_bot/app.py
+++ interesting_projects/Job_telegram_bot/app.py
@@ -13,28 +13,17 @@ bot = telebot.TeleBot(TOKEN)
 buttons = {
     'Djinni': ['Djinni Junior Python', 'Djinni Trainee Python'],
     'DOU': ['DOU Junior Python', 'DOU Trainee Python'],
-    'LinkedIn': ['LinkedIn Junior Python', 'LinkedIn Trainee Python'],
-    'Jooble': ['Jooble Junior Python', 'Jooble Trainee Python'],
-    'GRC': ['GRC Junior Python', 'GRC Trainee Python'],
     'Work': ['Work Junior Python', 'Work Trainee Python'],
-    'Jobs': ['Jobs Junior Python', 'Jobs Trainee Python']
 }
 
 urls = {
+    "Djinni Python": "https://djinni.co/jobs/?keywords=Python",
     "Djinni Junior Python": "https://djinni.co/jobs/?keywords=Junior+Python",
     "Djinni Trainee Python": "https://djinni.co/jobs/?keywords=Trainee+Python",
     "DOU Junior Python": "https://jobs.dou.ua/vacancies/?search=junior+python",
     "DOU Trainee Python": "https://jobs.dou.ua/vacancies/?search=trainee+python",
-    "LinkedIn Junior Python": "https://www.linkedin.com/jobs/search/?geoId=90010216&keywords=junior%20python&location=Kyiv%20Metropolitan%20Area",
-    "LinkedIn Trainee Python": "https://www.linkedin.com/jobs/search/?geoId=90010216&keywords=trainee%20python&location=Kyiv%20Metropolitan%20Area",
-    "Jooble Junior Python": "https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=junior%20python",
-    "Jooble Trainee Python": "https://ua.jooble.org/SearchResult?p=2&rgns=%D0%9A%D0%B8%D1%97%D0%B2&ukw=trainee%20python",
-    "GRC Junior Python": "https://grc.ua/search/vacancy?text=junior+python&from=suggest_post&fromSearchLine=true&area=115",
-    "GRC Trainee Python": "https://grc.ua/search/vacancy?text=trainee+python&from=suggest_post&fromSearchLine=true&area=115",
     "Work Junior Python": "https://www.work.ua/jobs-kyiv-junior+python/",
     "Work Trainee Python": "https://www.work.ua/jobs-kyiv-trainee+python/",
-    "Jobs Junior Python": "https://jobs.ua/vacancy/kiev/rabota-junior-python",
-    "Jobs Trainee Python": "https://jobs.ua/vacancy/kiev/rabota-trainee-python"
 }
 
 
@@ -63,7 +52,7 @@ def get_buttons(message, site, buttons):
     bot.send_message(message.chat.id, f"{site} buttons", reply_markup=markup)
 
 
-def parse(message, site, url):
+def parse(message, site, url, many=False):
     headers = {"User-Agent":"Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"}
     soup = BeautifulSoup(get_page(url, headers), "html.parser")
 
@@ -72,30 +61,38 @@ def parse(message, site, url):
             list_ = parse_djinni(soup)
         case 'DOU':
             list_ = parse_dou(soup)
-        case 'LinkedIn':
-            list_ = parse_linkedin(soup)
-        case 'Jooble':
-            list_ = parse_jooble(soup)
-        case 'GRC':
-            list_ = parce_grc(soup)
         case 'Work':
             list_ = parce_work(soup)
-        case 'Jobs':
-            list_ = parce_jobs(soup)
 
-    if not list_:
-        bot.send_message(message.chat.id, 'No vacancies')
-    else:
-        text = "\n\n".join(["\n".join(i) for i in list_])
-        bot.send_message(message.chat.id, text)
+        case 'Popular':
+            list_ = []
+
+            for url in [
+                "Djinni Junior Python",
+                "Djinni Trainee Python",
+                "DOU Junior Python",
+                "DOU Trainee Python",
+                "GRC Junior Python",
+                "GRC Trainee Python",
+                "Jobs Junior Python"
+            ]:
+                list_.append(parse(message, url.split()[0], urls[url]))
+            print(list_)
+    
+    if not many:
+        if not list_:
+            bot.send_message(message.chat.id, 'No vacancies')
+        else:
+            text = "\n\n".join(["\n".join(i) for i in list_])
+            bot.send_message(message.chat.id, text)
 
 
 # Parsers
-def parse_djinni(soup):
+def parse_djinni(soup, n=10):
     vacancies = []
     results = soup.find_all("li", class_="list-jobs__item")
 
-    for i in results[:10]:
+    for i in results[:n]:
         vacancies.append([
             i.find("div", class_="list-jobs__title")
             .find("a", class_="profile").find("span").text,
@@ -113,11 +110,11 @@ def parse_djinni(soup):
     return vacancies
 
 
-def parse_dou(soup):
+def parse_dou(soup, n=10):
     vacancies = []
     results = soup.find_all("div", class_="vacancy")
 
-    for i in results[:10]:
+    for i in results[:n]:
         vacancies.append([
             i.find("div", class_="title").find("a").text.replace("\xa0", " "),
             i.find("div", class_="title").find("a")["href"],
@@ -128,60 +125,11 @@ def parse_dou(soup):
     return vacancies
 
 
-def parse_linkedin(soup):
-    vacancies = []
-    results = soup.find_all("li", {"class": "ember-view jobs-search-results__list-item occludable-update p0 relative"})
-    print(results)
-
-    for i in results[:10]:
-        vacancies.append([
-            # TODO will do it later
-            # Parse from linekdin with selenium
-        ])
-
-    return vacancies
-
-
-def parse_jooble(soup):
-    vacancies = []
-    results = soup.find_all("article", {"data-test-name": "_jobCard"})
-
-    for i in results[:10]:
-        vacancies.append([
-            i.find("header").find("div").find("h2").find("a").text,
-
-            i.find("header").find("div").find("h2").find("a")["href"],
-            
-            i.find("section").find("div", class_="_15xYk4")
-            .find("div").find("div").find("div").find("p").text,
-
-            i.find("section").find("div", class_="_15xYk4")
-            .find("div").find_all("div")[2].find("div").text
-        ])
-
-    return vacancies
-
-
-def parce_grc(soup):
-    vacancies = []
-    results = soup.find_all("div", {"class": "vacancy-serp-item-body"})
-
-    for i in results[:10]:
-        vacancies.append([
-            i.find_all("a", {"class": "bloko-link"})[0].text,
-            i.find_all("a", {"class": "bloko-link"})[0]["href"],
-            i.find_all("a", {"class": "bloko-link"})[1].text,
-            i.find("div", {"data-qa": "vacancy-serp__vacancy-address"}).text
-        ])
-
-    return vacancies
-
-
-def parce_work(soup):
+def parce_work(soup, n=10):
     vacancies = []
     results = soup.find_all("div", {"class": "job-link"})
 
-    for i in results[:10]:
+    for i in results[:n]:
         vacancies.append([
             i.find("h2").text.strip(),
             "https://www.work.ua" + i.find("h2").find("a")["href"],
@@ -190,22 +138,6 @@ def parce_work(soup):
         ])
 
     return vacancies
-
-def parce_jobs(soup):
-    vacancies = []
-    results = soup.find_all("li", {"class": "b-vacancy__item js-item_list"})
-
-    for i in results[:10]:
-        vacancies.append([
-            i.find("a", {"class": "b-vacancy__top__title js-item_title"}).text,
-            i.find("a", {"class": "b-vacancy__top__title js-item_title"})["href"],
-            i.find("span", {"class": "b-vacancy__tech__item"}).text.strip(),
-            i.find("span", {"class": "b-vacancy__tech__item b-vacancy__tech__item-city"}).text.strip()
-        ])
-
-    return vacancies
-
-
 
 
 # Main bot's function
@@ -216,6 +148,9 @@ def main(message):
 
     if message.text in urls:
         parse(message, message.text.split()[0], urls[message.text])
+
+    if message.text == 'From popular sites':
+        parse(message, 'Popular', urls, many=True)
 
 
 bot.polling(none_stop=True)
