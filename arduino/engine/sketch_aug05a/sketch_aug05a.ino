cd899369e5d89b33c59b4450ababf5e25e4b202a
EdMix <ua.capitan@gmail.com>
2022-08-07 11:19:29 +0300

    Arduino - code for engine

--- arduino/engine/sketch_aug05a/sketch_aug05a.ino
+++ arduino/engine/sketch_aug05a/sketch_aug05a.ino
@@ -1,3 +1,5 @@
+CheapStepper stepper28BYJ_1(4,5,6,7);
+
 void setup() {
   pinMode(7, OUTPUT);
 
@@ -8,4 +10,7 @@ void loop() {
   delay(1000);
   digitalWrite(7, LOW);
   delay(1000); 
+
+  stepper28BYJ_1.setRpm(5);
+  stepper28BYJ_1.move(false, 4096);
 }
