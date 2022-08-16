40f0ea3e751dd523a69c40281beb67758a337ec6
EdMix <ua.capitan@gmail.com>
2022-08-16 12:39:19 +0300

    IVB - final version of application

--- interesting_projects/Irregular_verbs_bot/app.py
+++ interesting_projects/Irregular_verbs_bot/app.py
@@ -1,6 +1,8 @@
+import json
 import telebot
 import keys
 import random
+from telebot import types
 
 TOKEN = keys.TOKEN
 
@@ -8,6 +10,34 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    bot.send_message(message.chat.id, '')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("/new"))
+
+    bot.send_message(message.chat.id, 'Hi! This is bot for learning irregular verbs \
+at English. For start click at button.', reply_markup=markup)
+
+@bot.message_handler(commands=['new'])
+def start_bot(message):
+    with open('verbs.json', 'r') as file:
+        verbs = json.load(file)
+    verb = verbs[random.choice(list(verbs.keys()))]
+    bot.send_message(message.chat.id, verb)
+
+@bot.message_handler(content_types=['text'])
+def start_bot(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("/new"))
+
+    with open('verbs.json', 'r') as file:
+        verbs = json.load(file)
+
+    text = " - ".join(message.text.split()).lower()
+    right_text = " - ".join(verbs[text.split()[0]]).lower()
+
+    if text == right_text:
+        result_text = f"✅ {text} ✅"
+    else:
+        result_text = f"❌ {text} ❌\n✅ {right_text} ✅"
+    bot.send_message(message.chat.id, result_text, reply_markup=markup)
 
 bot.polling(none_stop=True)
