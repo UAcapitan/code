39ec7bcfcd3e7942621fd7f502df4e68f05ec142
EdMix <ua.capitan@gmail.com>
2022-08-16 10:30:05 +0300

    IVB - telebot message handler

--- interesting_projects/Irregular_verbs_bot/app.py
+++ interesting_projects/Irregular_verbs_bot/app.py
@@ -1,4 +1,13 @@
 import telebot
 import keys
+import random
 
 TOKEN = keys.TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    bot.send_message(message.chat.id, '')
+
+bot.polling(none_stop=True)
