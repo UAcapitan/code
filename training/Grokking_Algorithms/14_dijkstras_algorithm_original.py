1deb6d2105da14f71935ecd8530d09527384afe0
EdMix <ua.capitan@gmail.com>
2022-08-17 15:20:25 +0300

    Algorithms - original Dijkstra's algorithm

--- training/Grokking_Algorithms/14_dijkstras_algorithm_original.py
+++ training/Grokking_Algorithms/14_dijkstras_algorithm_original.py
@@ -16,6 +16,7 @@ graph = {
 }
 
 def algorithm(graph, start):
+    # Costs
     infinity = float("inf")
     costs = {
         i:infinity for i in graph.keys() if i != start
@@ -23,16 +24,42 @@ def algorithm(graph, start):
     for i in graph[start].keys():
         costs[i] = graph[start][i]
 
+    # Parents
     parents = {
         i:None for i in graph.keys() if i != start
     }
-
     for i in graph[start].keys():
         parents[i] = start
 
+    # Processed
     processed = []
 
-    return
+    # Main part of algorithm
+
+    def find_lowest(costs):
+        lowest_costs = float("inf")
+        lowest_costs_node = None
+        for node in costs:
+            cost = costs[node]
+            if cost < lowest_costs and node not in processed:
+                lowest_cost = cost
+                lowest_costs_node = node
+            return lowest_costs_node
+
+    node = find_lowest(costs)
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.append(node)
+        node = find_lowest(costs)
+
+    return costs, parents
 
 if __name__ == "__main__":
-    print(algorithm(graph, "start"))
+    for i in algorithm(graph, "start"):
+        print(i)
