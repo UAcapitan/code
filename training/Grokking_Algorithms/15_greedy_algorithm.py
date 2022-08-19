aa9a1fdc672ae4cbf13ed6dd05bdf82cfedb0f63
EdMix <ua.capitan@gmail.com>
2022-08-19 13:53:18 +0300

    Algorithm - greedy algorithm: preparing

---
+++ training/Grokking_Algorithms/15_greedy_algorithm.py
@@ -0,0 +1,15 @@
+states_needed = {"mt", "wa", "or", "id", "nv", "ut", "ca", "az"}
+
+stations = {
+    "kone": {"id", "nv", "ut"},
+    "ktwo": {"wa", "id", "mt"},
+    "kthree": {"or", "nv", "ca"},
+    "kfour": {"nv", "ut"},
+    "kfive": {"ca", "az"}
+}
+
+def algorithm(needed, stations):
+    pass
+
+if __name__ == "__main__":
+    algorithm()
