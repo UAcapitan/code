463a3937e443ae5cd7b1236a45f54d5c01c51187
EdMix <ua.capitan@gmail.com>
2022-08-19 19:19:08 +0300

    Algorithms - greedy algorithm: working

--- training/Grokking_Algorithms/15_greedy_algorithm.py
+++ training/Grokking_Algorithms/15_greedy_algorithm.py
@@ -9,7 +9,26 @@ stations = {
 }
 
 def algorithm(needed, stations):
-    pass
+    final_stations = set()
+    while needed:
+        print('-'*7)
+        best_station = None
+        states_covered = set()
+        for station, states in stations.items():
+            print(station, states)
+            covered = needed & states
+            print(covered)
+            if len(covered) > len(states_covered):
+                best_station = station
+                states_covered = covered
+                print(best_station, states_covered)
+
+        needed -= states_covered
+        final_stations.add(best_station)
+        print(final_stations)
+
+    return final_stations
+
 
 if __name__ == "__main__":
-    algorithm()
+    print(algorithm(states_needed, stations))
