70653696d60515a14468eb350c60b557f0cad96d
EdMix <ua.capitan@gmail.com>
2022-08-21 10:49:05 +0300

    Algorithms - dynamic programming: working

--- training/Grokking_Algorithms/17_dynamic_programming.py
+++ training/Grokking_Algorithms/17_dynamic_programming.py
@@ -1,7 +1,7 @@
 items = {
     "guitar": [1500, 1],
     "music-player": [3000, 4],
-    "laptop": [2000, 3]
+    "laptop": [2000, 3],
 }
 
 size = 4
@@ -12,12 +12,16 @@ def algorithm(items, size):
         list_ = []
         for i in range(1, size + 1):
             if data[1] <= i:
-                list_.append(data[0])
+                n = i - data[1]
+                if n > 0 and len(cell) > 0:
+                    list_.append(data[0] + cell[-1][n-1])
+                else:
+                    list_.append(data[0])
             else:
-                list_.append(cell[-1][i-2])
+                list_.append(cell[-1][i-1])
         cell.append(list_)
 
-    return cell
+    return cell[-1][-1]
 
 if __name__ == "__main__":
     print(algorithm(items, size))
