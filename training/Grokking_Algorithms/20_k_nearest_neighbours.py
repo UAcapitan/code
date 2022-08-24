432cb716f39247de888c37e094ee807722fde8a0
EdMix <ua.capitan@gmail.com>
2022-08-25 00:08:46 +0300

    Algorithms - k nearest neighbours

---
+++ training/Grokking_Algorithms/20_k_nearest_neighbours.py
@@ -0,0 +1,29 @@
+# Orange color - 1
+# Green color - 10
+# Little size - 1
+# Big size - 10
+
+# [name, color, size]
+fruits = [
+    ['tangerine', 2, 2],
+    ['tangerine', 1, 1],
+    ['tangerine', 1, 3],
+    ['tangerine', 3, 3],
+    ['tangerine', 4, 2],
+    ['tangerine', 2, 5],
+    ['tangerine', 5, 2],
+
+    ['apple', 6, 10],
+    ['apple', 7, 9],
+    ['apple', 9, 5],
+    ['apple', 10, 7],
+    ['apple', 8, 9],
+    ['apple', 5, 7],
+    ['apple', 7, 5],
+]
+
+def algorithm():
+    pass
+
+if __name__ == "__main__":
+    print(algorithm())
