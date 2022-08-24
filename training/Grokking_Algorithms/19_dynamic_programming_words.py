7ee700e35502cad9da8104d6764cc813ed35cb0f
EdMix <ua.capitan@gmail.com>
2022-08-24 15:34:39 +0300

    Algorithms - dynamic programming: generally

---
+++ training/Grokking_Algorithms/19_dynamic_programming_words.py
@@ -0,0 +1,25 @@
+words = ['test', 'rest']
+
+def algorithm(words):
+    word1, word2 = words
+    cell = []
+    for i in range(len(word1)):
+        list_ = []
+        for j in range(len(word2)):
+            if word1[i] == word2[j]:
+                if i == 0:
+                    list_.append(1)
+                else:
+                    list_.append(cell[-1][i-1] + 1)
+            else:
+                if i == 0 or len(list_) == 0:
+                    list_.append(0)
+                else:
+                    list_.append(list_[-1])
+        cell.append(list_)
+
+
+    return max(cell)[0]
+
+if __name__ == "__main__":
+    print(algorithm(words))
