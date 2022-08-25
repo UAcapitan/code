f9fb086934b7004f57149cb4e47996bb17958c00
EdMix <ua.capitan@gmail.com>
2022-08-26 00:09:07 +0300

    Selenium - parse from site

--- training/Selenium_video_course/app_2.py
+++ training/Selenium_video_course/app_2.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Settings for FireFox
 option = webdriver.FirefoxOptions()
@@ -8,4 +9,15 @@ option.set_preference('media.volume_scale', '0.0')
 
 browser = webdriver.Firefox(options=option)
 browser.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
-browser.close()
+browser.quit()
+
+browser = webdriver.Chrome()
+browser.get("https://nick-name.ru/generate/")
+
+list_ = []
+
+for _ in range(10):
+    xpath = '/html/body/div[1]/div[1]/div[1]/div[2]/form/table/tbody/tr[5]/td[2]/input'
+    browser.find_element(By.XPATH, xpath).click()
+
+browser.quit()
