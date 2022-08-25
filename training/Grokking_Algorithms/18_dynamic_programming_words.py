3bb0077a1621a2144435d3e078a2f3f1c4998d2b
EdMix <ua.capitan@gmail.com>
2022-08-25 10:02:45 +0300

    Algorithms - dynamical programming: the longest string

--- training/Grokking_Algorithms/18_dynamic_programming_words.py
+++ training/Grokking_Algorithms/18_dynamic_programming_words.py
@@ -1,5 +1,4 @@
-# words = ['test', 'rest']
-words = ['tttpppaaa', 'tctpppnaa']
+words = ['test', 'rest']
 
 def algorithm(words):
     word1, word2 = words
@@ -8,19 +7,21 @@ def algorithm(words):
         list_ = []
         for j in range(len(word2)):
             if word1[i] == word2[j]:
-                if i == 0:
+                if i == 0 or j == 0:
                     list_.append(1)
                 else:
-                    list_.append(cell[-1][i-1] + 1)
+                    list_.append(cell[-1][j-1] + 1)
             else:
-                if i == 0 or len(list_) == 0:
-                    list_.append(0)
-                else:
-                    list_.append(list_[-1])
+                list_.append(0)
         cell.append(list_)
 
+    result = []
+    for i in cell:
+        result += i
 
-    return cell[-1][-1]
+    return cell, max(result)
 
 if __name__ == "__main__":
-    print(algorithm(words))
+    table, max_ = algorithm(words)
+    print(table)
+    print(max_)
