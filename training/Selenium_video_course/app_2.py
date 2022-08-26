c79d58b909503b8d4a898280320521061892fec5
EdMix <ua.capitan@gmail.com>
2022-08-26 11:48:28 +0300

    Selenium - parsing is done and yet some options

--- training/Selenium_video_course/app_2.py
+++ training/Selenium_video_course/app_2.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 # Settings for FireFox
 option = webdriver.FirefoxOptions()
+option.set_preference('general.useragent.override', 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0')
 option.set_preference('dom.webdriver.enabled', False)
 option.set_preference('dom.webnotifications.enabled', False)
 option.set_preference('media.volume_scale', '0.0')
@@ -11,7 +12,12 @@ browser = webdriver.Firefox(options=option)
 browser.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
 browser.quit()
 
-browser = webdriver.Chrome()
+# Settings for Chrome
+option = webdriver.ChromeOptions()
+option.headless = True
+
+# Parsing nicknames from site
+browser = webdriver.Chrome(options=option)
 browser.get("https://nick-name.ru/generate/")
 
 list_ = []
@@ -19,5 +25,11 @@ list_ = []
 for _ in range(10):
     xpath = '/html/body/div[1]/div[1]/div[1]/div[2]/form/table/tbody/tr[5]/td[2]/input'
     browser.find_element(By.XPATH, xpath).click()
+
+    xpath = "/html/body/div/div[1]/div[1]/div[2]/form/table/tbody/tr[6]/td[2]/input"
+    list_.append(browser.find_element(By.XPATH, xpath).get_attribute("value"))
+
+for link in list_:
+    print(link)
 
 browser.quit()
