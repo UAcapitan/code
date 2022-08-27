07b0e7e4ed4dbbaf849cd1e83a68c5578b206f29
EdMix <ua.capitan@gmail.com>
2022-08-27 15:48:36 +0300

    Experiments - context manager realization

--- training/Experiments/2_context_manager_realization.py
+++ training/Experiments/2_context_manager_realization.py
@@ -4,4 +4,23 @@ with open('text.txt', 'w') as file:
     file.write(text)
 
 # My realization
-pass
+class ContextManager():
+    def __init__(self, file):
+        self.file = file
+        print("File was opened")
+
+    def __enter__(self):
+        self.file = open(self.file, "w")
+        print("Enter is working")
+        return self.file
+
+    def __exit__(self, type, value, traceback):
+        self.file.close()
+        del self.file
+        print(type)
+        print(value)
+        print(traceback)
+        print("File was closed")
+
+with ContextManager("text.txt") as file:
+    file.write("Some text manager")
