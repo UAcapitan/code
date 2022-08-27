e3311e619a92aedd705ca9ce4c77abc4ce93c772
EdMix <ua.capitan@gmail.com>
2022-08-28 00:07:21 +0300

    Experiments - annotation and mypy

---
+++ training/Experiments/4_annotation.py
@@ -0,0 +1,20 @@
+
+from typing import Union
+
+def test(x: int, y: int, z: int) -> int:
+    return x*y*z
+
+def add_text(data: int) -> list:
+    return [f"Result: {str(data)}", data]
+
+def result(data: int) -> list[int]:
+    return [int(i) for i in str(data)]
+
+if __name__ == "__main__":
+    n_list: list[int] = [10,10,70]
+    number: int = test(*n_list)
+    text_list: list = add_text(number)
+    data: list[int] = result(text_list[1])
+
+    print(text_list[0])
+    print(data)
