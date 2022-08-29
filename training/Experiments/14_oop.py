ef3c22266196a5a44b58f189c349d3f63560d897
EdMix <ua.capitan@gmail.com>
2022-08-30 02:27:59 +0300

    Experiments - oop

---
+++ training/Experiments/14_oop.py
@@ -0,0 +1,34 @@
+
+# Creating class
+class FirstClass:
+    def __init__(self):
+        self.n: int = 3
+    
+    # EncapsulationNone
+    def __add_test(self, new: int) -> int:
+        self.n += 1
+        self.new: int = new
+        return self.n
+
+# Inheritance
+class SecondClass(FirstClass):
+    def __init__(self) -> None:
+        super().__init__()
+
+# Polymorphism
+class ThirdClass(SecondClass):
+    def __add_test(self, new: int) -> int:
+        new += 1
+        self.n += new - 2
+        return self.n
+
+    def get_result(self, new: int) -> int:
+        return self.__add_test(new)
+
+if __name__ == "__main__":
+    obj_1 = FirstClass()
+    
+    obj_2 = SecondClass()
+
+    obj_3 = ThirdClass()
+    print(obj_3.get_result(1))
