885e357aa0f13cdcdc58665fbde5c8660ef6bf78
EdMix <ua.capitan@gmail.com>
2022-08-31 00:29:22 +0300

    Experiments - cookies from site

--- training/Selenium_video_course/4_cookies.py
+++ training/Selenium_video_course/4_cookies.py
@@ -1,5 +1,16 @@
+
+import time
+import pickle
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+browser.get("https://www.work.ua/")
+time.sleep(100)
+
+pickle.dump(browser.get_cookies(), open("session", "wb"))
+
+cookies = pickle.load(open("session", "rb"))
+print(cookies)
 
 browser.quit()
