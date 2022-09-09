3f04d9ada37483cde044e5a036f7290b71c0bad6
EdMix <ua.capitan@gmail.com>
2022-09-10 00:01:12 +0300

    Experiments - types of copies

--- training/Experiments/5_types_of_copies.py
+++ training/Experiments/5_types_of_copies.py
@@ -1,16 +1,24 @@
 
+import copy
+
+
 list_original: list[int] = [1,2,3,4,5,6,7,8,9]
 
 if __name__ == "__main__":
+
+    # Copy
+    list_copy: list[int] = list_original
+
     # Shallow copy
-    list_shallow_copy: list[int] = list_original
+    list_shallow_copy: list[int] = list_original.copy()
 
     # Deep copy
-    list_deep_copy: list[int] = list_original.copy()
+    list_deep_copy: list[int] = copy.deepcopy(list_original)
 
     list_original[0] = 10
-    list_shallow_copy[-1] = 11
+    list_copy[-1] = 11
     
     print(list_original)
+    print(list_copy)
     print(list_shallow_copy)
     print(list_deep_copy)
