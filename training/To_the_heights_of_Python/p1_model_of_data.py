fbe2cc4d4a7daeb409464932e8619a1f4970de20
EdMix <ua.capitan@gmail.com>
2022-09-20 03:02:45 +0300

    THP - model of data: Vector full

--- training/To_the_heights_of_Python/p1_model_of_data.py
+++ training/To_the_heights_of_Python/p1_model_of_data.py
@@ -1,6 +1,38 @@
 
+import random
+
+
+# Tests
 def test_vector():
     v1 = Vector(2, 5)
     v2 = Vector(2, 2)
 
-    assert v1 + v2 == Vector(4, 7)
+    assert v1 == 'Vector(2,5)'
+    assert v2 == 'Vector(2,2)'
+    assert v1 + v2 == 'Vector(4,7)'
+    assert v1 + v2 == Vector(4,7)
+
+# Code
+class Vector:
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
+    
+    def __add__(self, obj):
+        x = self._x + obj._x
+        y = self._y + obj._y
+        return Vector(x,y)
+
+    def __str__(self):
+        print("str")
+        return f'Vector({self._x},{self._y})'
+
+    def __repr__(self):
+        print(repr)
+        return f'Vector({self._x},{self._y})'
+
+    def __eq__(self, obj):
+        try:
+            return (self._x == obj._x) and (self._y == obj._y)
+        except:
+            return str(self) == obj
