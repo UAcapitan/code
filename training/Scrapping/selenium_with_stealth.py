4267790adae8e88eca95a1e1746cb3ce52d58128
EdMix <ua.capitan@gmail.com>
2022-10-01 00:01:01 +0300

    Scrapping - selenium with stealth

---
+++ training/Scrapping/selenium_with_stealth.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+from selenium_stealth import stealth
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+# options.add_argument("--headless")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+with webdriver.Chrome(options=options) as driver:
+        stealth(driver,
+                user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,
+                run_on_insecure_origins=False
+                )
+
+        driver.get("https://bot.sannysoft.com/")
+        input()
