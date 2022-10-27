78f7dade72a9b3ccf9e68ea1fb706852c4ff2e21
EdMix <ua.capitan@gmail.com>
2022-10-28 00:01:23 +0300

    PMF - blog, update login and reg, template

--- interesting_projects/ProtectMyFreedom/app.py
+++ interesting_projects/ProtectMyFreedom/app.py
@@ -1,6 +1,7 @@
 
 import os
-import hashlib
+import models
+import functional as fn
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,15 +13,6 @@ app.secret_key = "1234"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db.init_app(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(512))
-
-def encrypte_password(password):
-    return hashlib.md5(password.encode("utf_8")).hexdigest()
 
 @app.route("/")
 def index():
@@ -39,18 +31,19 @@ def reg():
         if password != re_password:
             return "Password are not similar"
 
-        username_exist = User.query.filter_by(username=username).first()
+        username_exist = models.User.query.filter_by(username=username).first()
         if username_exist != None:
             return 'Username is already used'
 
-        email_exist = User.query.filter_by(email=email).first()
+        email_exist = models.User.query.filter_by(email=email).first()
         if email_exist != None:
             return 'Email is already used'
 
-        password = encrypte_password(password)
+        password = fn.encrypte_password(password)
 
         try:
-            db.session.add(User(username=username, email=email, password=password))
+            # fn.add_to_db(models.User(username=username, email=email, password=password))
+            db.session.add(models.User(username=username, email=email, password=password))
             db.session.commit()
             return redirect("/login")
         except:
@@ -64,9 +57,9 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        password = encrypte_password(password)
+        password = fn.encrypte_password(password)
 
-        user = User.query.filter_by(email=email).first()
+        user = models.User.query.filter_by(email=email).first()
 
         if user.password == password:
             session['loggedin'] = True
@@ -87,8 +80,20 @@ def logout():
 
 @app.route("/main")
 def main():
-    return render_template("main.html")
+    if session['loggedin']:
+        return render_template("main.html")
+    else:
+        return redirect("/login")
 
+@app.route("/blog")
+def blog():
+    if session['loggedin']:
+        return render_template("blog.html")
+    return redirect("/login")
+
+@app.route("/ask")
+def ask():
+    return render_template("ask.html")
 
 if __name__ == "__main__":
     if not os.path.isfile("db.sqlite"):
